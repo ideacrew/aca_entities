1cb8859a55be725ac922137cc2b0f504e923be25
Dan Thomas <dan@ideacrew.com>
2021-02-02 15:56:56 -0500

    Rename Entry entity.  Clean up Glossary and Entry attributes and documentation

---
+++ lib/aca_entities/questionnaires/entry.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Questionnaire
+
+    # A defined term and definition pair that appears in a Questionnaire's Glossary
+    class Entry < Dry::Struct
+
+      # @!attribute [r] key
+      # An unambiguous reference to a term. The key is used as a token to
+      # reference where an term should appear in a rendered {Questionnaire}
+      # @return [String]
+      attribute :key, Types::String.meta(omittable: false)
+
+      # @!attribute [r] term
+      # The human-readable word or phrase to define
+      # At runtime this value is substituted inline where a matching token is encountered
+      # @return [String]
+      attribute :term, Types::String.meta(omittable: false)
+
+      # @!attribute [r] definition
+      # A statement that represents the concept and essential nature of the term
+      # @return [String]
+      attribute :definition, Types::String.meta(omittable: false)
+
+    end
+  end
+end
--- lib/aca_entities/questionnaires/glossary.rb
+++ lib/aca_entities/questionnaires/glossary.rb
@@ -1,30 +1,26 @@
 # frozen_string_literal: true
 
 module AcaEntities
-  module Entities
-    module Shared
-      module Questionnaire
+  module Questionnaire
 
-        # A Glossary is a dictionary that contains a list of terms and their definitions
-        class Glossary < Dry::Struct
+    # A Glossary is a dictionary that contains a list of terms and their definitions
+    class Glossary < Dry::Struct
 
-          # @!attribute [r] title
-          # A name given to the resource by which the resource is formally known
-          # @return [String]
-          attribute :title,             Types::String.meta(omittable: true)
+      # @!attribute [r] title
+      # A name given to the resource by which the resource is formally known
+      # @return [String]
+      attribute :title,             Types::String.meta(omittable: true)
 
-          # @!attribute [r] description
-          # An optional account of the content of this resource
-          # @return [String]
-          attribute :description,       Types::String.optional.meta(ommitable: true)
+      # @!attribute [r] description
+      # An optional account of the content of this resource
+      # @return [String]
+      attribute :description,       Types::String.optional.meta(ommitable: true)
 
-          # @!attribute [r] description
-          # A collection of term/definition pairs
-          # @return [Array<GlossaryEntry>]
-          attribute :glossary_entries,  Types::Array.of(GlossaryEntry).optional.meta(ommitable: true)
-        end
-
-      end
+      # @!attribute [r] entries
+      # A collection of term/definition pairs
+      # @return [Array<GlossaryEntry>]
+      attribute :entries,  Types::Array.of(Entry).optional.meta(ommitable: true)
     end
+
   end
 end
