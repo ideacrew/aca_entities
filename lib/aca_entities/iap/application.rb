2ca1683244a903c426414e849d46eda6b2857b8d
Dan Thomas <dan@ideacrew.com>
2021-03-02 12:59:23 -0500

    Add entities from EA

--- lib/aca_entities/iap/application.rb
+++ lib/aca_entities/iap/application.rb
@@ -3,5 +3,24 @@
 module Iap
   class Application < Dry::Struct
 
+    # FFM Attributes
+      # Application ID
+      # Identification Category Text
+      # Application Signature Date
+      # Original Signature Date
+      # Application Creation Date
+      # Application Update Date
+      # Applying for Financial Assistance Indicator
+      # Medicaid Determination Indicator
+      # Privacy Policy Agreement Indicator
+      # Attested Not Incarcerated Indicator
+      # Pending Disposition
+      # Attested If Information Changes Indicator
+      # Attested Medicaid Applicant Awareness Indicator
+      # Attested Non Perjury Indicator
+      # Attested To Cooperate With Medical Collection Agency
+      # Tax Return Access Indicator
+      # Tax Return Access
+
   end
 end
