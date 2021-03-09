dbd147adb0961a38a5f85c4546730641c2e7c6ce
ymhari <hariym471@gmail.com>
2021-03-09 12:56:01 -0500

    ref: 177209747 Fixes ruboocp issue.

--- spec/payment_transactions/contracts/transaction_contract_spec.rb
+++ spec/payment_transactions/contracts/transaction_contract_spec.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
-require "../aca_entities/lib/aca_entities/payment_transactions/contracts/transaction_contract.rb"
+
+require "../aca_entities/lib/aca_entities/payment_transactions/contracts/transaction_contract"
 
 RSpec.describe ::AcaEntities::PaymentTransactions::Contracts::TransactionContract,  dbclean: :after_each do
 
   describe "Transaction contract" do
 
     context "calling with params values as nil" do
-      let(:invalid_params) { {enrollment_id: '', carrier_id: '', enrollment_effective_date: ''} }
-      let(:error_message) { {:carrier_id=>["Carrier id is blank"], :enrollment_effective_date=>["must be a date"], :enrollment_id=>["Enrollment id is blank"]} }
+      let(:invalid_params) { { enrollment_id: '', carrier_id: '', enrollment_effective_date: '' } }
+      let(:error_message) do
+        { :carrier_id => ["Carrier id is blank"],
+          :enrollment_effective_date => ["must be a date"],
+          :enrollment_id => ["Enrollment id is blank"] }
+      end
 
       it 'should be a container-ready operation' do
         expect(subject.respond_to?(:call)).to be_truthy
@@ -25,7 +30,11 @@ RSpec.describe ::AcaEntities::PaymentTransactions::Contracts::TransactionContrac
     context "sending with missing keys should return errors" do
 
       let(:invalid_params) { {} }
-      let(:error_message) { {:carrier_id=>["is missing"], :enrollment_effective_date=>["is missing"], :enrollment_id=>["is missing"]} }
+      let(:error_message) do
+        { :carrier_id => ["is missing"],
+          :enrollment_effective_date => ["is missing"],
+          :enrollment_id => ["is missing"] }
+      end
 
       it 'should throw errors' do
         expect(subject.call(invalid_params).errors.to_h).to eq error_message
@@ -34,8 +43,11 @@ RSpec.describe ::AcaEntities::PaymentTransactions::Contracts::TransactionContrac
 
     context "sending with all keys and values should not errors" do
 
-      let(:valid_params) { {enrollment_id: "test", carrier_id: "test", enrollment_effective_date: Date.today} }
-
+      let(:valid_params) do
+        { :enrollment_id => "test",
+          :carrier_id => "test",
+          :enrollment_effective_date => Date.today }
+      end
 
       it 'should return Dry::Validation::Result object' do
         expect(subject.call(valid_params)).to be_a ::Dry::Validation::Result
