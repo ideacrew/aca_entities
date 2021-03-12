feb994c4aa9a8d62caf26513e53ab96f2e8704e3
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-03-12 17:01:20 -0500

    Refs #CV3_setup, Add PayPeriodInformation entity and contract for HouseholdMember with tests.

---
+++ lib/aca_entities/contracts/pay_period_information_contract.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Contracts
+    # Contract for PayPeriodInformation.
+    class PayPeriodInformationContract < Dry::Validation::Contract
+
+      params do
+        optional(:end_date).maybe(:date)
+        optional(:hours_worked_in_pay_period).maybe(:integer)
+        optional(:data_source_income_type_amount).maybe(:float)
+      end
+    end
+  end
+end
