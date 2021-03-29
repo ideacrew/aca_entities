55f7d43c72dd4c9c0459e6f1c4b3d3be81690c5b
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-03-29 17:13:15 -0400

    Refs #CV3_IAP, added AttestationContract with tests.

---
+++ lib/aca_entities/app_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  # A module for all the common methods that can be used across the gem.
+  module AppHelper
+    # An object is blank if it's empty, or a whitespace string.
+    # For example, nil, '', '   ', [], and {} are all blank.
+    # Note: false is not blank.
+    def check_if_blank?(data_object)
+      data_object.respond_to?(:empty?) ? data_object.strip.empty? : data_object.nil?
+    end
+
+    def check_if_present?(data_object)
+      !is_blank?(data_object)
+    end
+  end
+end
