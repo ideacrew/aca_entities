d97152f77b04f15690fa4385694e58d5089c5a21
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-03-29 17:19:39 -0400

    Refs #CV3_IAP, change validation of is_disabled from optional to required.

--- lib/aca_entities/magi_medicaid/contracts/attestation_contract.rb
+++ lib/aca_entities/magi_medicaid/contracts/attestation_contract.rb
@@ -10,19 +10,12 @@ module AcaEntities
         params do
           required(:is_applying_coverage).filled(:bool)
           optional(:is_incarcerated).maybe(:bool)
-          optional(:is_disabled).maybe(:bool)
+          # is_self_attested_disabled
+          required(:is_disabled).filled(:bool)
           optional(:is_self_attested_long_term_care).maybe(:bool)
         end
 
         rule(:is_incarcerated) do
-          if key? && values[:is_applying_coverage] == true && check_if_blank?(value)
-            # This field is mandatory if person is applying for coverage in the UI.
-            # XSD validation: <xs:element name="is_incarcerated" type="xs:boolean" minOccurs="0"/>
-            key.failure(text: 'cannot be blank')
-          end
-        end
-
-        rule(:is_disabled) do
           if key? && values[:is_applying_coverage] == true && check_if_blank?(value)
             # This field is mandatory if person is applying for coverage in the UI.
             # XSD validation: <xs:element name="is_incarcerated" type="xs:boolean" minOccurs="0"/>
