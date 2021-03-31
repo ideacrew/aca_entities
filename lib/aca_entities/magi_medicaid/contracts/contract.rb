02e8a82309c7c2d3d5c8cddb3fce5457ed4b5198
Dan Thomas <dan@ideacrew.com>
2021-03-31 15:04:20 -0400

    Add MAGI Medicaid Library.  Add FederalPovertyLevel entity

---
+++ lib/aca_entities/magi_medicaid/contracts/contract.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module MagiMedicaid
+    module Contracts
+      # Configuration values and shared rules and macros for {AcaEntities::Medicaid} validation contracts
+      class Contract < Dry::Validation::Contract
+        # config.messages.backend - the localization backend to use. Supported values are: :yaml and :i18n
+        # config.messages.backend = :i18n
+        # config.messages.default_locale - default I18n-compatible locale identifier
+        # config.messages.default_locale = :en
+        # config.messages.load_paths - an array of files paths that are used to load messages
+        # config.messages.top_namespace - the key in the locale files under which messages are defined, by default it's dry_validation
+        # config.messages.namespace - custom messages namespace for a contract class. Use this to differentiate common messages
+
+        rule(:effective_end_on, :effective_start_on) do
+          if values[:effective_end_on] < values[:effective_start_on]
+            key.failure('must be after effective_start_on')
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/aca_entities/magi_medicaid/contracts/federal_poverty_level_contract.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module MagiMedicaid
+    module Contracts
+      # Schema and validation rules for {AcaEntities::MagiMedicaid::FederalPoveryLevel}
+      class FederalPovertyLevelContract < AcaEntities::MagiMedicaid::Contracts::Contract
+        # @!method call(opts)
+        # @param [Hash] opts the parameters to validate using this contract
+        # @option opts [String] :state_code required
+        # @option opts [Integer] :household_size required
+        # @option opts [BigDecimal] :annual_poverty_guideline required
+        # @option opts [BigDecimal] :add_person_annual_amount required
+        # @option opts [BigDecimal] :monthly_poverty_guideline required
+        # @option opts [BigDecimal] :add_person_monthly_amount required
+        # @option opts [Date] :effective_start_on required
+        # @option opts [Date] :effective_end_on required
+        # @return [Dry::Monads::Result]
+        params do
+          required(:state_code).filled(:string)
+          required(:household_size).filled(:integer)
+          required(:annual_poverty_guideline).filled(AcaEntities::Types::Money)
+          required(:add_person_annual_amount).filled(AcaEntities::Types::Money)
+          required(:monthly_poverty_guideline).filled(AcaEntities::Types::Money)
+          required(:add_person_monthly_amount).filled(AcaEntities::Types::Money)
+          required(:effective_start_on).filled(:date)
+          required(:effective_end_on).filled(:date)
+        end
+      end
+    end
+  end
+end
