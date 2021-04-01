6b9dc9979b8568e8ec95d62006061063021de36c
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-04-01 15:12:21 -0400

    Refs #CV3_IAP, remove unwanted entities and contracts under MagiMedicaid namespace.

--- lib/aca_entities/magi_medicaid/person_reference.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AcaEntities
-  module MagiMedicaid
-    class PersonReference < Dry::Struct
-      attribute :person_id,  Types::Integer.meta(omitttable: false)
-    end
-  end
-end
