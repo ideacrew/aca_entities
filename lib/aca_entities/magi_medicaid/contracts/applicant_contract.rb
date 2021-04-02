b38762df233ff6202650e182cc5e9d33a0e2219c
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-04-02 16:04:51 -0400

    Refs #CV3_IAP, add ApplicantContract with tests.

--- lib/aca_entities/magi_medicaid/contracts/applicant_contract.rb
+++ lib/aca_entities/magi_medicaid/contracts/applicant_contract.rb
@@ -4,87 +4,301 @@ module AcaEntities
   module MagiMedicaid
     module Contracts
       # Contract for Applicant
+      # rubocop:disable Metrics/ClassLength
       class ApplicantContract < Dry::Validation::Contract
-
         params do
-          optional(:name_pfx).maybe(:string)
-          required(:first_name).maybe(:string)
-          optional(:middle_name).maybe(:string)
-          required(:last_name).maybe(:string)
-          optional(:name_sfx).maybe(:string)
-          required(:ssn).maybe(:string)
-          required(:gender).maybe(:string)
-          required(:dob).filled(:date)
-
-          optional(:is_primary_applicant).maybe(:bool)
-          optional(:person_hbx_id).maybe(:string)
-          optional(:family_member_id).maybe(:string)
-
-          optional(:is_incarcerated).maybe(:bool)
-          optional(:is_disabled).filled(:bool)
-          optional(:ethnicity).maybe(:array)
-          optional(:race).maybe(:string)
-          optional(:indian_tribe_member).maybe(:bool)
-          optional(:tribal_id).maybe(:string)
-
-          optional(:language_code).maybe(:string) # Fix Me
-          optional(:no_dc_address).filled(:bool) # Fix Me
-          optional(:is_homeless).maybe(:bool)
-          optional(:is_temporarily_out_of_state).maybe(:bool)
-
-          optional(:vlp_subject).maybe(:string)
-          optional(:vlp_description).maybe(:string)
-          optional(:alien_number).maybe(:string)
-          optional(:i94_number).maybe(:string)
-          optional(:visa_number).maybe(:string)
-          optional(:passport_number).maybe(:string)
-          optional(:sevis_id).maybe(:string)
-          optional(:naturalization_number).maybe(:string)
-          optional(:receipt_number).maybe(:string)
-          optional(:citizenship_number).maybe(:string)
-          optional(:card_number).maybe(:string)
-          optional(:country_of_citizenship).maybe(:string)
-          optional(:expiration_date).maybe(:date)
-          optional(:issuing_country).maybe(:string)
-          optional(:relationship).maybe(:string)
-
-          optional(:no_ssn).maybe(:string)
-          required(:citizen_status).maybe(:string)
-          required(:is_consumer_role).filled(:bool)
-          optional(:same_with_primary).maybe(:bool)
+          required(:name).filled(:hash)
+          required(:identifying_information).filled(:hash)
+          required(:demographic).filled(:hash)
+          required(:attestation).filled(:hash)
+          required(:is_primary_applicant).filled(:bool)
+          optional(:native_american_information).maybe(:hash)
+          optional(:citizenship_immigration_status_information).maybe(:hash)
+          optional(:is_consumer_role).maybe(:bool)
+          optional(:is_resident_role).maybe(:bool)
           required(:is_applying_coverage).filled(:bool)
+          optional(:is_consent_applicant).maybe(:bool)
+          optional(:vlp_document).maybe(:hash)
+          required(:family_member_reference).filled(:hash)
+          required(:person_hbx_id).filled(:string)
+          required(:is_required_to_file_taxes).filled(:bool)
+          optional(:is_joint_tax_filing).maybe(:bool)
+          optional(:is_claimed_as_tax_dependent).maybe(:bool)
+          optional(:claimed_as_tax_dependent_by).maybe(:hash)
+          optional(:tax_filer_kind).maybe(Types::TaxFilerKind)
+          optional(:student).maybe(:hash)
+          optional(:is_refugee).maybe(:bool)
+          optional(:is_trafficking_victim).maybe(:bool)
+          optional(:foster_care).maybe(:hash)
+          required(:pregnancy_information).filled(:hash)
+          optional(:is_subject_to_five_year_bar).maybe(:bool)
+          optional(:is_five_year_bar_met).maybe(:bool)
+          optional(:is_forty_quarters).maybe(:bool)
+          optional(:is_ssn_applied).maybe(:bool)
+          optional(:non_ssn_apply_reason).maybe(:string)
+          optional(:moved_on_or_after_welfare_reformed_law).maybe(:bool)
+          optional(:is_currently_enrolled_in_health_plan).maybe(:bool)
+          optional(:has_daily_living_help).maybe(:bool)
+          optional(:need_help_paying_bills).maybe(:bool)
 
-          optional(:addresses).maybe(:array)
-          optional(:phones).maybe(:array)
-          optional(:emails).maybe(:array)
+          # :incomes
+          # Does this person have income from an employer(wages, tips, bonuses, etc.) in 2021?
+          required(:has_job_income).filled(:bool)
+          # Does this person expect to receive self-employment income in 2021?
+          required(:has_self_employment_income).filled(:bool)
+          # Did this person receive Unemployment Income at any point in 2021?
+          required(:has_unemployment_income).filled(:bool)
+          # Does this person expect to have income from other sources in 2021?
+          required(:has_other_income).filled(:bool)
+
+          # :deductions
+          # Does this person expect to have income adjustments in 2021?
+          required(:has_deductions).filled(:bool)
+
+          # :benefits(Health Benefit Coverages)
+          # Is this person currently enrolled in health coverage?
+          required(:has_enrolled_health_coverage).filled(:bool)
+          # Does this person currently have access to other health coverage that they are not enrolled in,
+          # including coverage they could get through another person?
+          required(:has_eligible_health_coverage).filled(:bool)
+
+          optional(:addresses).array(:hash)
+          optional(:emails).array(:hash)
+          optional(:phones).array(:hash)
+          optional(:incomes).array(:hash)
+          optional(:benefits).array(:hash)
+          optional(:deductions).array(:hash)
+        end
+
+        rule(:name) do
+          if key? && value
+            if value.is_a?(Hash)
+              result = AcaEntities::Contracts::PersonNameContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid name', error: result.errors.to_h)
+              else
+                values.merge!(name: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid name. Expected a hash.')
+            end
+          end
+        end
+
+        rule(:identifying_information) do
+          if key? && value
+            if value.is_a?(Hash)
+              result = IdentifyingInformationContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid identifying_information', error: result.errors.to_h)
+              else
+                values.merge!(identifying_information: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid identifying_information. Expected a hash.')
+            end
+          end
+        end
+
+        rule(:demographic) do
+          if value.is_a?(Hash)
+            result = DemographicContract.new.call(value)
+            if result&.failure?
+              key.failure(text: 'invalid demographic', error: result.errors.to_h)
+            else
+              values.merge!(demographic: result.to_h)
+            end
+          else
+            key.failure(text: 'invalid demographic. Expected a hash.')
+          end
+        end
+
+        rule(:attestation) do
+          if value.is_a?(Hash)
+            result = AttestationContract.new.call(value)
+            if result&.failure?
+              key.failure(text: 'invalid attestation', error: result.errors.to_h)
+            else
+              values.merge!(attestation: result.to_h)
+            end
+          else
+            key.failure(text: 'invalid attestation. Expected a hash.')
+          end
+        end
+
+        rule(:native_american_information) do
+          if key? && value
+            if value.is_a?(Hash)
+              result = NativeAmericanInformationContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid native_american_information', error: result.errors.to_h)
+              else
+                values.merge!(native_american_information: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid native_american_information. Expected a hash.')
+            end
+          end
+        end
+
+        rule(:citizenship_immigration_status_information) do
+          if key? && value
+            if value.is_a?(Hash)
+              result = CitizenshipImmigrationStatusInformationContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid citizenship_immigration_status_information', error: result.errors.to_h)
+              else
+                values.merge!(citizenship_immigration_status_information: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid citizenship_immigration_status_information. Expected a hash.')
+            end
+          end
+        end
+
+        # rule(:family_member_reference) do
+        #   if key? && value
+        #     if value.is_a?(Hash)
+        #       result = AcaEntities::Contracts::References::FamilyMemberContract.new.call(value)
+        #       if result&.failure?
+        #         key.failure(text: 'invalid family_member_reference', error: result.errors.to_h)
+        #       else
+        #         values.merge!(family_member_reference: result.to_h)
+        #       end
+        #     else
+        #       key.failure(text: 'invalid family_member_reference. Expected a hash.')
+        #     end
+        #   end
+        # end
+
+        rule(:vlp_document) do
+          if key? && value
+            if value.is_a?(Hash)
+              result = VlpDocumentContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid vlp_document', error: result.errors.to_h)
+              else
+                values.merge!(vlp_document: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid vlp_document. Expected a hash.')
+            end
+          end
+        end
+
+        rule(:is_claimed_as_tax_dependent) do
+          key.failure(text: 'cannot be blank') if key? && values[:is_required_to_file_taxes] && check_if_blank?(value)
+        end
+
+        rule(:claimed_as_tax_dependent_by) do
+          if values[:is_required_to_file_taxes] && values[:is_claimed_as_tax_dependent]
+            if check_if_blank?(value)
+              key.failure(text: 'cannot be blank')
+            elsif value.is_a?(Hash)
+              result = ApplicantReferenceContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid claimed_as_tax_dependent_by.', error: result.errors.to_h)
+              else
+                values.merge!(claimed_as_tax_dependent_by: result.to_h)
+              end
+            end
+          end
+        end
+
+        rule(:student) do
+          if key? && values[:is_applying_coverage]
+            if value.is_a?(Hash)
+              result = StudentContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid student', error: result.errors.to_h)
+              else
+                values.merge!(student: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid student. Expected a hash.')
+            end
+          end
+        end
+
+        rule(:foster_care) do
+          if key? && values[:is_applying_coverage]
+            if value.is_a?(Hash)
+              result = FosterCareContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid foster_care', error: result.errors.to_h)
+              else
+                values.merge!(foster_care: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid foster_care. Expected a hash.')
+            end
+          end
+        end
+
+        rule(:pregnancy_information) do
+          if key? && value
+            if value.is_a?(Hash)
+              result = PregnancyInformationContract.new.call(value)
+              if result&.failure?
+                key.failure(text: 'invalid pregnancy_information', error: result.errors.to_h)
+              else
+                values.merge!(pregnancy_information: result.to_h)
+              end
+            else
+              key.failure(text: 'invalid pregnancy_information. Expected a hash.')
+            end
+          end
+        end
+
+        def any_income?(values)
+          values[:has_job_income] || values[:has_self_employment_income] || values[:has_unemployment_income] || values[:has_other_income]
+        end
+
+        rule(:incomes).each do
+          if key? && value && any_income?(values)
+            if value.is_a?(Hash)
+              result = IncomeContract.new.call(value)
+              key.failure(text: "invalid income", error: result.errors.to_h) if result&.failure?
+            else
+              key.failure(text: "invalid incomes. Expected a hash.")
+            end
+          end
+        end
+
+        rule(:deductions).each do
+          if key? && value && values[:has_deductions]
+            if value.is_a?(Hash)
+              result = DeductionContract.new.call(value)
+              key.failure(text: "invalid deduction", error: result.errors.to_h) if result&.failure?
+            else
+              key.failure(text: "invalid deductions. Expected a hash.")
+            end
+          end
+        end
+
+        def any_benefit?(values)
+          values[:has_enrolled_health_coverage] || values[:has_eligible_health_coverage]
+        end
+
+        rule(:benefits).each do
+          if key? && value && any_benefit?(values)
+            if value.is_a?(Hash)
+              result = BenefitContract.new.call(value)
+              key.failure(text: "invalid benefit", error: result.errors.to_h) if result&.failure?
+            else
+              key.failure(text: "invalid benefits. Expected a hash.")
+            end
+          end
         end
 
         rule(:addresses).each do
           if key? && value
             if value.is_a?(Hash)
-              result = AddressContract.new.call(value)
+              result = AcaEntities::Contracts::AddressContract.new.call(value)
               key.failure(text: "invalid address", error: result.errors.to_h) if result&.failure?
             else
               key.failure(text: "invalid addresses. Expected a hash.")
             end
-          end
-        end
-
-        rule(:ssn, :no_ssn) do
-          key.failure(text: "ssn is missing") if values[:ssn].empty? && values[:no_ssn] == '0'
-        end
-
-        rule(:is_primary_applicant) do
-          if key? && value
-            key.failure(text: "family_member_id should be present") if values[:family_member_id].blank?
-            key.failure(text: "person hbx id should be present") if values[:person_hbx_id].blank?
-          end
-        end
-
-        rule(:is_applying_coverage) do
-          if values[:is_applying_coverage]
-            key.failure(text: "Incarceration question must be answered") if values[:is_incarcerated].to_s.blank?
-            key.failure(text: "Indian tribe member question must be answered") if values[:indian_tribe_member].to_s.blank?
           end
         end
 
@@ -110,6 +324,7 @@ module AcaEntities
           end
         end
       end
+      # rubocop:enable Metrics/ClassLength
     end
   end
 end
