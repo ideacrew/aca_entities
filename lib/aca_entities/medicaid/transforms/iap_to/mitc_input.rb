18f84147251ebdf7c1dc731904a6877eac842b4f
Vishal Kalletla <vishuk1201@gmail.com>
2021-04-06 18:44:42 -0400

    add maps list

--- lib/aca_entities/medicaid/transforms/iap_to/mitc_input.rb
+++ lib/aca_entities/medicaid/transforms/iap_to/mitc_input.rb
@@ -1,11 +1,127 @@
 # This file defines the maps
 module Medicaid
   module Transforms
-    module IapTo
-      class MitcInput < ::Operations::Transform
+    module IapTo #MCRTo
+      class MitcInput < ::Operations::Transform  #CvInput
         include ::Transform::Transformer
+        #import AcaEntities::Mcr::Transformations
+         # transform functions
 
         record_delimiter 'applications.identifier.result' # TODO: support wild card ex. applications.*.result (prefer Regex)
+        # TODO namespace_map "source || output"
+
+        namespace 'attestations' do
+          rewrap 'family' do
+            map '', 'hbx_id'
+            map 'renewEligibilityYearQuantity', 'renewal_consent_through_year' #, -> {|year | year + value_of("attestations.application.applicationSignatures")}
+            map '', 'vlp_documents_status'
+            map '', 'min_verifications_due_date'
+            map '', 'special_enrollment_periods'
+            map '', 'irs_groups'
+            map '', 'broker_agency_accounts'
+            map '', 'general_agency_accounts'
+            map '', 'documents'
+            map '', 'payment_transactions'
+            map '', 'financial_assistance_applications'
+            rewrap 'family_members', type: :array do
+              map '', 'is_primary_applicant'
+              map '', 'is_coverage_applicant'
+              map '', 'is_consent_applicant'
+
+              namespace 'attestations.application.contactInformation' do
+                rewrap 'person' do
+                  namespace 'email' do
+                    rewrap 'emails' , type: :array do
+                      map '', 'kind'
+                      map 'email', 'address'
+                    end
+                  end
+                  namespace 'primaryPhoneNumber'  do
+                    rewrap 'phones', type: :array do
+                      map 'type', 'kind'#,  -> {|value|  value.to_s.downcase }
+                      map '', 'country_code'
+                      #map "number","" , ->(area_code: 3, number: 7) {|value| value}
+                      map 'number (1..3)', 'area_code'#, -> {|value| value}
+                      map 'number (4..10)', 'number'
+                      map 'ext', 'extension'
+                      map '', 'primary'
+                    end
+                end
+              end
+#
+                        
+
+              namespace 'members.*.demographic' do
+                rewrap 'person' do
+                  rewrap 'names' do
+                    map '', 'names'
+                    map 'firstName', 'first_name'
+                    map 'lastName', 'last_name'
+                  end
+                  map 'ssn', 'encrypted_ssn'
+                  map 'birthDate', 'dob'
+                  map 'sex', 'gender'
+                  map 'computed.members.*.ssnStatusReason', 'no_ssn'
+                  map '', 'hbx_id'
+                  map 'blindOrDisabledIndicator', 'is_disabled'
+                  map '', 'ethnicity'
+                  map '', 'race'
+                  map '', 'tribal_id'
+                  map '', 'language_code' , "default: en"
+                  map 'noHomeAddressIndicator', 'is_homeless'
+                  map 'liveOutsideStateTemporarilyIndicator', 'is_temporarily_out_of_state'
+
+                  namespace 'mailingAddress' do
+                    rewrap 'addresses', type: :array do
+                      map '', 'has_fixed_address'
+                      map 'mailingAddress', 'kind'
+                      map 'streetName1', 'address_1'
+                      map '', 'address_2'
+                      map 'cityName', 'city'
+                      map 'countyName', 'county'
+                      map 'countyFipsCode', 'county_code'
+                      map 'stateCode', 'state'
+                      map 'zipCode', 'zip'
+                      map 'countryCode', 'country_name'
+                      map '', 'validation_status'
+                      map '', 'start_on'
+                      map '', 'end_on'
+                      map 'liveOutsideStateTemporarilyIndicator', 'lives_outside_state_temporarily'
+                    end
+                  end
+
+                  namespace "homeAddress" do
+                    rewrap 'addresses', type: :array do
+                      map '', 'has_fixed_address'
+                      map 'homeAddress', 'kind'
+                      map 'streetName1', 'address_1'
+                      map '', 'address_2'
+                      map 'cityName', 'city'
+                      map 'countyName', 'county'
+                      map 'countyFipsCode', 'county_code'
+                      map 'stateCode', 'state'
+                      map 'zipCode', 'zip'
+                      map 'countryCode', 'country_name'
+                      map '', 'validation_status'
+                      map '', 'start_on'
+                      map '', 'end_on'
+                      map 'liveOutsideStateTemporarilyIndicator', 'lives_outside_state_temporarily'
+                    end
+                  end
+                end
+              end
+              map '', 'person_relationships'
+              map '', 'relative_hbx_id'
+              map '', 'kind'
+              map '', 'hbx_id'
+            end
+
+            map '', 'households'
+            map '', 'coverage_households'
+            map '', 'tax_households'
+          end
+        end
+
 
         # map "dateofBirth", "Age", 'age_on' #-> { age_on('1/1/2000') }
 
@@ -77,26 +193,43 @@ module Medicaid
           map "sourceSystemName", "sourceName"
         end
 
-        namespace "attestations", "records" do
-          namespace "application", "record" do
+        namespace "attestations.application" do
+          namespace "application" do
+            rewrap "verifications.consumer_role" do
+              map "comments", "user_comments"
+              map "contactInformation", "contact"
+              map "applicationAssistors", "applicationAssistors"
+            end
             map "legalAttestations", "attestations"
             map "applicationSignatures", "signatures"
-
-            # rewrap "verifications.consumer_role" do
-            #   map "comments", "user_comments"
-            #   map "contactInformation", "contact"
-            #   map "applicationAssistors", "applicationAssistors"
-            # end
           end
         end
 
-        namespace "records.record" do
-          rewrap "verifications.consumer_role" do
-          	map "comments", "user_comments"
-          	map "contactInformation", "contact"
-          	map "applicationAssistors", "applicationAssistors"
-          end
-        end
+
+        #
+        # namespace "attestations", "records" do
+        #   namespace "application", "record" do
+        #     map "legalAttestations", "attestations"
+        #     map "applicationSignatures", "signatures"
+        #   end
+        # end
+        #
+        #
+        # namespace "records.record" do
+        #   rewrap "verifications.consumer_role" do
+        #     map "comments", "user_comments"
+        #     map "contactInformation", "contact"
+        #     map "applicationAssistors", "applicationAssistors"
+        #   end
+        # end
+
+        # namespace "records.record" do
+        #   rewrap "verifications.consumer_role" do
+        #   	map "comments", "user_comments"
+        #   	map "contactInformation", "contact"
+        #   	map "applicationAssistors", "applicationAssistors"
+        #   end
+        # end
 
         # namespace "records" do
         #   namespace "record" do
@@ -173,7 +306,6 @@ module Medicaid
         # map "attestations.application.contactInformation", "verifications.consumer_role.contact", [:rename_nested_keys]
 
 
-
         # map "lastUpdateMetadata.roleId", "lastUpdateData.role", :rename_nested_keys
 
         # map "attestations", "verifications", [:rename_keys]
@@ -183,7 +315,6 @@ module Medicaid
 
         # map "computed.application.bestSEP", "computed.seps.bestSEP", [:rewrap_keys]
         # map "attestations.application.applicationSignatures", "verifications.consumer_role.signatures", [:rename_nested_keys]
-
 
 
         # "applicationSignatures"=>[{"applicationSignatureText"=>"Roslyn dshab"}, {"applicationSignatureType"=>"APPLICANT"}, {"applicationSignatureDate"=>"2019-01-04"}],
