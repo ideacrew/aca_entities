174c7f3883a807d0045575594811bd22a77bf1fb
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-04-06 15:52:00 -0400

    Refs #CV3_IAP, include MitcLibrary and IapLibrary in MagiMedicaidLibrary.

--- lib/aca_entities/libraries/magi_medicaid_library.rb
+++ lib/aca_entities/libraries/magi_medicaid_library.rb
@@ -4,7 +4,9 @@
 module AcaEntities
   module Libraries
     module MagiMedicaidLibrary
-      require 'aca_entities/magi_medicaid/types'
+      require 'aca_entities/magi_medicaid/libraries/mitc_library'
+      require 'aca_entities/magi_medicaid/libraries/iap_library'
+
       require 'aca_entities/magi_medicaid/contracts/contract'
       require 'aca_entities/magi_medicaid/contracts/federal_poverty_level_contract'
       require 'aca_entities/magi_medicaid/contracts/create_federal_poverty_level_contract'
