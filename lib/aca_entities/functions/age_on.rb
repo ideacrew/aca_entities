177da31020a7b7a14400d1800544d4bd93673712
Vishal Kalletla <vishuk1201@gmail.com>
2021-04-14 13:33:39 -0400

    add specs

--- lib/aca_entities/functions/age_on.rb
+++ lib/aca_entities/functions/age_on.rb
@@ -14,28 +14,38 @@ module AcaEntities
 
       private
 
+      # @api private
       def to_proc
         ->(arg1) {age_on(arg1)}
       end
 
       # Convert dob to age
       #
-      # @param dob The input dob
-      # @param on_date The age on the given date
+      # @param dob The input dob, either date as string or date as object
+      # @param on_date The input date, either date as string or date as object
+      #
+      # @example
+      #   dob: "1980-1-1", on_date: "2021-1-1"
+      #   # =>  41
+      #
+      # @return age
       #
       # @example
       #   dob: Date.new(1980,1,1), on_date: Date.new(2021,1,1)
       #   # =>  41
       #
       # @return age
+      #
+      # @api private
       def age_on(dob, on_date: @on_date || Date.today)
-        dob = t(:to_date)[dob]
-        on_date = t(:to_date)[on_date]
+        dob =  (t(:to_string).>>t(:to_date))[dob]
+        on_date =  (t(:to_string).>>t(:to_date))[on_date]
         age = on_date.year - dob.to_date.year
         return age - 1 unless on_date.month > dob.month || (on_date.month == dob.month && on_date.day >= dob.day)
         age
       end
 
+      # @api private
       def t(*args)
         AcaEntities::Functions::AgeOn[*args]
       end
