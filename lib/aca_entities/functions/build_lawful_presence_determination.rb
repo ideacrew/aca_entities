9be00204256d55a34def358da221fc862dba8853
Vishal Kalletla <vishuk1201@gmail.com>
2021-05-06 17:57:01 -0400

    ref transform: update functions to construct hash

--- lib/aca_entities/functions/build_lawful_presence_determination.rb
+++ lib/aca_entities/functions/build_lawful_presence_determination.rb
@@ -29,10 +29,19 @@ module AcaEntities
       # ALIEN_LAWFULLY_PRESENT_STATUS = 'alien_lawfully_present'
       # INELIGIBLE_CITIZEN_VERIFICATION = %w[not_lawfully_present_in_us non_native_not_lawfully_present_in_us].freeze
 
-      def call(input)
-        hash = { input.resolve('noAlienNumberIndicator').name => input.resolve('noAlienNumberIndicator').item,
-                 input.resolve('citizenshipIndicator').name => input.resolve('citizenshipIndicator').item,
-                 input.resolve('naturalizedCitizenIndicator').name => input.resolve('naturalizedCitizenIndicator').item }
+      def call(input, m_identifier = nil)
+        member = input.find(/attestations.members.(\w+)$/)&.map(&:item)&.last
+        member_identifier = m_identifier || member
+
+        noAlienNumberIndicator = input.find(Regexp.new("noAlienNumberIndicator.#{member_identifier}"))&.first
+        citizenshipIndicator = input.find(Regexp.new("citizenshipIndicator.#{member_identifier}"))&.first
+        naturalizedCitizenIndicator = input.find(Regexp.new("naturalizedCitizenIndicator.#{member_identifier}"))&.first
+
+        hash = {
+          noAlienNumberIndicator&.name.split('.').first => noAlienNumberIndicator.item,
+          citizenshipIndicator&.name.split('.').first => citizenshipIndicator.item,
+          naturalizedCitizenIndicator&.name.split('.').first => naturalizedCitizenIndicator.item
+        }
 
         val = hash.keys.find { |_k| hash.key(true) }
         assign_citizen_status(val)
