1a76358fe3dd2527ac86157c8bc2a3291d478488
Trey Evans <lewis.r.evans@gmail.com>
2021-05-15 15:44:59 -0400

    Code cleanup while checking XML stuff.

---
+++ lib/aca_entities/medicaid/atp.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require_relative "atp/transfer_header"
+require_relative "atp/sender"
+require_relative "atp/receiver"
+require_relative "atp/application_metadata"
+require_relative "atp/attestation"
+require_relative "atp/insurance_application"
+require_relative "atp/household_composition"
+require_relative "atp/household_income"
+require_relative "atp/medicaid_household"
+require_relative "atp/address"
+require_relative "atp/citizenship_immigration_status_information"
+require_relative "atp/demographic"
+require_relative "atp/identifying_information"
+require_relative "atp/identifying_numbers_from_document"
+require_relative "atp/immigration_document"
+require_relative "atp/native_american_information"
+require_relative "atp/employer_sponsored_insurance_information"
+require_relative "atp/lowest_cost_plan"
+require_relative "atp/current_income"
+require_relative "atp/ssa_income_information"
+require_relative "atp/ssa_information"
+require_relative "atp/ssa_work_quarter"
+require_relative "atp/medicaid_specific_information"
+require_relative "atp/other_insurance_coverage_information"
+require_relative "atp/other_insurance_detail"
+require_relative "atp/person_description_identifier"
+require_relative "atp/coverage_specific_information"
+require_relative "atp/coverage_period"
+require_relative "atp/relationship"
+require_relative "atp/foster_care"
+require_relative "atp/dhs_save"
+require_relative "atp/basis_for_outcome"
+require_relative "atp/person_name"
+require_relative "atp/applicant"
+require_relative "atp/account_transfer_request"
+
+module AcaEntities
+  module Medicaid
+    module Atp
+    end
+  end
+end
