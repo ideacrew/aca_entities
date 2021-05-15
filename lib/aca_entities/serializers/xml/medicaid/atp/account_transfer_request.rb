a5cbe35f371b366470f68350ab54c5c1350597e6
Trey Evans <lewis.r.evans@gmail.com>
2021-05-15 19:40:32 -0400

    Initial happymapper example.

---
+++ lib/aca_entities/serializers/xml/medicaid/atp/account_transfer_request.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Serializers
+    module Xml
+      module Medicaid
+        module Atp
+          class AccountTransferRequest
+            include HappyMapper
+            register_namespace 'ex', 'http://at.dsh.cms.gov/exchange/1.0'
+            register_namespace 'ext', 'http://at.dsh.cms.gov/extension/1.0'
+
+            tag 'AccountTransferRequest'
+            namespace 'ex'
+
+            attribute :version, String, tag: "atVersionText", namespace: "ext"
+
+            has_one :transfer_header, TransferHeader 
+            has_many :senders, Sender
+            has_many :receivers, Receiver
+            has_one :insurance_application, InsuranceApplication
+            has_many :people, Person
+            has_many :physical_households, PhysicalHousehold
+
+
+            def self.domain_to_mapper(account_transfer_request)
+              mapper = self.new
+              mapper.version = "2.4"
+              mapper.senders = [Sender.domain_to_mapper(account_transfer_request.sender)]
+              mapper.receivers = [Receiver.domain_to_mapper(account_transfer_request.receiver)]
+              mapper.transfer_header = TransferHeader.domain_to_mapper(account_transfer_request.transfer_header)
+              mapper.insurance_application = InsuranceApplication.domain_to_mapper(account_transfer_request.insurance_application)
+              mapper.people = [Person.domain_to_mapper(account_transfer_request.person)]
+              mapper.physical_households = [PhysicalHousehold.domain_to_mapper(account_transfer_request.physical_household)]
+              mapper
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/aca_entities/serializers/xml/medicaid/atp/account_transfer_request_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'aca_entities/medicaid/atp'
+require 'aca_entities/serializers/xml'
+
+RSpec.describe AcaEntities::Serializers::Xml::Medicaid::Atp::AccountTransferRequest do
+  let(:account_transfer_request) do
+    AcaEntities::Medicaid::Atp::AccountTransferRequest.new(atp_properties)
+  end
+
+  let(:atp_properties) do
+    {
+      transfer_header: transfer_header,
+      sender: {},
+      receiver: {},
+      physical_household: {
+        household_size_quantity: 1,
+        household_member_reference: []
+      },
+      insurance_application: insurance_application,
+      medicaid_household: {},
+      person: person
+    }
+  end
+
+  let(:transfer_header) do
+    {
+      transfer_id: "ABCDE",
+      transfer_date: DateTime.now,
+      number_of_referrals: 5,
+      recipient_code: "MedicaidCHIP"
+    }
+  end
+
+  let(:insurance_application) do
+    {
+      application_metadata: application_metadata,
+      attestation: attestation
+    }
+  end
+
+  let(:application_metadata) do
+    {
+      application_id: "an application id",
+      application_signature_date: DateTime.now,
+      creation_date: DateTime.now,
+      submission_date: DateTime.now,
+      identification_category_text: "ID CATEGORY TEXT",
+      financial_assistance_indicator: false,
+      medicaid_determination_indicator: false
+    }
+  end
+
+  let(:attestation) do
+    {
+      attested_if_information_changes_indicator: false,
+      attested_non_perjury_indicator: false,
+      tax_return_access_indicator: false
+    }
+  end
+
+  let(:person) do
+    {
+      person_name: {
+        first_name: "A FIRST NAME",
+        last_name: "A LAST NAME"
+      },
+      demographic: {
+        dob: Date.today,
+        gender: "A GENDER"
+      },
+      citizenship_immigration_status_information: {
+        us_citizen: true
+      },
+      native_american_information: {
+        is_native_american_or_alaska_native: false
+      }
+    }
+  end
+
+  let(:mapper) { described_class.domain_to_mapper(account_transfer_request) }
+  let(:schema) { Nokogiri::XML::Schema(File.open(schema_location)) }
+  let(:schema_location) do
+    loc = File.join(
+      File.dirname(__FILE__),
+      "..", "..", "..", "..", "..",
+      "reference", "xml", "atp",
+      "atp_service.xsd"
+    )
+    File.expand_path(loc)
+  end
+
+  it "should be schema valid" do
+    document = Nokogiri::XML(mapper.to_xml)
+    expect(schema.valid?(document)).to be_truthy
+  end
+end
