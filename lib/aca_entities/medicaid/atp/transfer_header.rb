4fc3f8e6767923a4d8ab6806ed3b60c79d425bcf
Trey Evans <lewis.r.evans@gmail.com>
2021-05-15 15:44:59 -0400

    Code cleanup while checking XML stuff.

--- lib/aca_entities/medicaid/atp/transfer_header.rb
+++ lib/aca_entities/medicaid/atp/transfer_header.rb
@@ -9,7 +9,7 @@ module AcaEntities
         attribute :transfer_date,          Types::DateTime
         attribute :number_of_referrals,    Types::Integer
         attribute :recipient_code,         Types::String
-        attribute :medicaid_chip_state,    Types::String.optional.meta(omittable: true)
+        attribute :state_code,             Types::String.optional.meta(omittable: true)
       end
     end
   end
