2d1dfe155561552ff79949fe9964857f9c2030f1
Trey Evans <lewis.r.evans@gmail.com>
2021-05-15 19:40:32 -0400

    Initial happymapper example.

---
+++ lib/aca_entities/serializers/xml.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'happymapper'
+require_relative "xml/medicaid"
+
+module AcaEntities
+  module Serializers
+    module Xml
+    end
+  end
+end
---
+++ lib/aca_entities/serializers/xml/medicaid.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "medicaid/atp"
+
+module AcaEntities
+  module Serializers
+    module Xml
+      module Medicaid
+      end
+    end
+  end
+end
