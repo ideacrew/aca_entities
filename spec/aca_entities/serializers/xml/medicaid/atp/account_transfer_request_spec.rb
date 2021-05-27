f501febf59693b5df953dea70e1188f45b25b33d
Trey Evans <lewis.r.evans@gmail.com>
2021-05-27 11:23:23 -0400

    Check business rules.

--- spec/aca_entities/serializers/xml/medicaid/atp/account_transfer_request_spec.rb
+++ spec/aca_entities/serializers/xml/medicaid/atp/account_transfer_request_spec.rb
@@ -3,6 +3,7 @@
 require 'spec_helper'
 require 'aca_entities/medicaid/atp'
 require 'aca_entities/serializers/xml/medicaid/atp'
+require 'open3'
 
 RSpec.describe AcaEntities::Serializers::Xml::Medicaid::Atp::AccountTransferRequest do
   let(:account_transfer_request) do
@@ -91,8 +92,40 @@ RSpec.describe AcaEntities::Serializers::Xml::Medicaid::Atp::AccountTransferRequ
     File.expand_path(loc)
   end
 
-  it "should be schema valid" do
+  let(:schematron_location) do
+    loc = File.join(
+      File.dirname(__FILE__),
+      "..", "..", "..", "..", "..",
+      "reference", "xml", "atp"
+    )
+    File.expand_path(loc)
+  end
+
+  let(:business_error_ns) do
+    {
+      svrl: "http://purl.oclc.org/dsdl/svrl"
+    }
+  end
+
+  it "is schema valid" do
     document = Nokogiri::XML(mapper.to_xml)
     expect(schema.valid?(document)).to be_truthy
+  end
+
+  it "passes business rule validation" do
+    data = mapper.to_xml
+    output, _err = Open3.capture3("java -jar atp_validator-0.1.0-jar-with-dependencies.jar --oneshot", stdin_data: data, binmode: true, chdir: schematron_location)
+    error_doc = Nokogiri::XML(output)
+    error_objects = error_doc.xpath("//svrl:failed-assert", business_error_ns).map do |node|
+      location = node.at_xpath("@location").content
+      message = node.at_xpath("svrl:text").content
+      [location, message]
+    end
+    error_objects.each do |error|
+      puts "\n\n======= Business Rule Failure ======="
+      puts error.first
+      puts error.last
+    end
+    expect(error_objects).to be_empty
   end
 end
