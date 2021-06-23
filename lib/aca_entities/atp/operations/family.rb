c1ec8a39561f040bf1452dae968839f65d053dbf
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-23 10:08:53 -0400

    ref# transforms: initial setup

---
+++ lib/aca_entities/atp/operations/family.rb
@@ -0,0 +1,69 @@
+require 'dry/monads'
+require 'dry/monads/do'
+
+module AcaEntities
+  module Atp
+    module Operations
+      # Family Operations for atp payloads
+      class Family
+        include Dry::Monads[:result, :do, :try]
+
+        def initialize(*args)
+          handler = args.first
+          @worker_mode = handler[:worker_mode] || :batch
+          @path = handler[:path] || nil
+          @source_hash = handler[:source_hash]
+        end
+
+        def call
+          # valid_params = yield validate_atp(source_hash)
+          params = yield xml_transform
+        #   params = yield batch_json_transform if @worker_mode == :batch # (app_params)
+          transform_result = yield single_json_transform(params) if @worker_mode == :single
+
+          Success(transform_result)
+        end
+
+        private
+
+        def validate_atp(params)
+        end
+
+        def input_xml
+          return nil if source_hash
+          source_path = path || default_xml_path
+          path = Pathname.pwd.join(source_path)
+          File.read(path)
+        end
+
+        def xml_transform
+          result = AcaEntities::Serializers::Xml::Medicaid::Atp::AccountTransferRequest.parse(input_xml)
+          Success(result.to_hash)
+        end
+
+        def single_json_transform(params)
+          record = ::AcaEntities::Atp::Transformers::Cv::Family.transform(params)
+          Successs(record)
+        end
+
+        # TODO: (app_params)
+        def transform_batch
+          result = []
+          input = file_path || source_hash # (app_params)
+
+          ::AcaEntities::Atp::Transformers::Cv::Family.call(input, { transform_mode: worker_mode }) do |payload|
+            record = worker_mode == :batch ? klass.transform(payload) : payload
+            # TODO: broken
+            result << ::AcaEntities::Ffe::Operations::McrTo::Family.new.call(record: record[:family])
+          end
+
+          Success(result)
+        end
+
+        def default_xml_path
+          'spec/support/atp_sample_payloads/sample.xml'
+        end
+      end
+    end
+  end
+end
--- lib/aca_entities/serializers/xml/medicaid/atp/person.rb
+++ lib/aca_entities/serializers/xml/medicaid/atp/person.rb
@@ -18,7 +18,7 @@ module AcaEntities
 
             element :us_citizen_indicator, Boolean, tag: "PersonUSCitizenIndicator", namespace: "nc"
             element :living_indicator, Boolean, tag: "PersonLivingIndicator", namespace: "nc"
-            element :ssn, String, tag: "PersonSSNIdentification", namespace: "nc"
+            element :ssn, String, tag: "PersonSSNIdentification/ns2:IdentificationID", namespace: "nc"
             element :sex, String, tag: "PersonSexText", namespace: "nc"
             element :race, String, tag: "PersonRaceText", namespace: "nc"
             element :ethnicity, String, tag: "PersonEthnicityText", namespace: "nc"
@@ -30,6 +30,18 @@ module AcaEntities
               mapper = self.new
               mapper.person_name = PersonName.domain_to_mapper(applicant)
               mapper
+            end
+
+            def to_hash
+              {
+                person_name: person_name.to_hash,
+                us_citizen_indicator: us_citizen_indicator,
+                living_indicator: living_indicator,
+                ssn: ssn,
+                sex: sex,
+                race: race,
+                ethnicity: ethnicity
+              }
             end
           end
         end
