df84e996e112d4416a7d60299c3578d9ad82ad43
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-23 10:08:53 -0400

    ref# transforms: initial setup

---
+++ lib/aca_entities/atp/transformers/cv/family.rb
@@ -0,0 +1,40 @@
+module AcaEntities
+  module Atp
+    module Transformers
+      module Cv
+        # Transformers implementation for atp payloads
+        class Family < ::AcaEntities::Operations::Transforms::Transform
+          include ::AcaEntities::Operations::Transforms::Transformer
+          
+          namespace 'people' do
+            rewrap 'family_members' do
+              namespace 'person_name' do
+                rewrap '', :array do
+                  map 'given', 'person.person_name.first_name'
+                  map 'middle', 'person.person_name.middle_name'
+                  map 'sur', 'person.person_name.last_namee'
+                  map 'full', 'person.person_name.full_name'
+                end
+              end
+
+              map 'us_citizen_indicator', 'person.consumer_role.lawful_presence_determination.citizen_status'
+              map 'living_indicator', 'person_demographics.is_homeless' #code to update 
+              map 'ssn', 'person_demographics.ssn'
+              map 'sex', 'person_demographics.gender'
+              add_key 'no_ssn'
+              add_key 'date_of_death'
+              add_key 'dob_check'
+              add_key 'is_incarcerated'
+              add_key 'tribal_id'
+              add_key 'language_code'
+
+              
+              map 'race', 'person.race'
+              map 'ethnicity', 'person.ethnicity'
+            end
+          end
+        end
+      end
+    end
+  end
+end
