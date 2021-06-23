e2c222c915d2963544cc19295b3004cf53d8b95f
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-23 17:44:18 -0400

    ref# transforms_atp: update family operation and spec

--- spec/aca_entities/apt/operations/family_spec.rb
+++ spec/aca_entities/apt/operations/family_spec.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'aca_entities/serializers/xml/medicaid/atp'
+require "aca_entities/atp/transformers/cv/family"
+require "aca_entities/atp/operations/family"
 
 RSpec.describe ::AcaEntities::Atp::Operations::Family, dbclean: :after_each do
-  let(:input_xml) {File.read(Pathname.pwd.join('spec/support/atp_sample_payloads/sample.xml'))}
+  let(:xml_path) {Pathname.pwd.join('spec/support/atp_sample_payloads/sample.xml')}
 
   describe 'with valid xml' do
     before do
-      @result = described_class.new(worker_mode: 'single', path: input_xml).call
+      @result = described_class.new(worker_mode: :single, path: xml_path).call
     end
 
     it 'should return transformed hash' do
