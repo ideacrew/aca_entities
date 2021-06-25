6e29b9ca43e0c321f812eb5c1e81d9a65e826de4
Kristin Merbach <kristin@kristinmerbach.com>
2021-06-25 08:38:30 -0400

    fix rubocop errors

--- lib/aca_entities/serializers/xml/medicaid/atp/contact_information.rb
+++ lib/aca_entities/serializers/xml/medicaid/atp/contact_information.rb
@@ -12,13 +12,13 @@ module AcaEntities
             tag 'ContactInformation'
             namespace 'hix-core'
             
-            #An electronic mailing address by which a person or organization may be contacted.
+            # An electronic mailing address by which a person or organization may be contacted.
             element :email_id, String, tag: "ContactEmailID", namespace: "nc"
 
-            #A postal address by which a person or organization may be contacted.
+            # A postal address by which a person or organization may be contacted.
             has_one :mailing_address, ContactMailingAddress
 
-            #A telephone number for a telecommunication device by which a person or organization may be contacted.
+            # A telephone number for a telecommunication device by which a person or organization may be contacted.
             has_one :telephone_number, ContactTelephoneNumber
 
             def self.domain_to_mapper(_contact_info)
