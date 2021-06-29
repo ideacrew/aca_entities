d2f63f3bd6c57b61d874f88e4573bdfbfced5aee
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-29 09:40:42 -0400

    ref# family transforms: update transform functions

--- lib/aca_entities/atp/functions/address_builder.rb
+++ lib/aca_entities/atp/functions/address_builder.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Layout/LineLength, Metrics/AbcSize, Metrics/ClassLength
 module AcaEntities
-    module Atp
-      module Functions
-        # build application and applicants
-        class AddressBuilder
-          def call(cache)
-            @memoized_data = cache
-            contacts_information = @memoized_data.find(Regexp.new('record.people.*.augementation')).map(&:item).last[:contacts]
+  module Atp
+    module Functions
+      # build application and applicants
+      class AddressBuilder
+        def call(cache)
+          @memoized_data = cache
+          contacts_information = @memoized_data.find(Regexp.new('record.people.*.augementation')).map(&:item).last[:contacts]
 
-            result = contacts_information.each_with_object([]) do |contact_info, collector|
-              address = contact_info.dig(:contact,:mailing_address,:address)
-              collector << address_hash(address, contact_info) if address
-            end
+          contacts_information.each_with_object([]) do |contact_info, collector|
+            next if contact_info[:category_code].downcase == 'residency'
+            address = contact_info.dig(:contact, :mailing_address, :address)
+            collector << address_hash(address, contact_info) if address
+          end
+        end
 
-            result
-          end
-    
-          private
-    
-          def address_hash(address, contact_info)
-            {
-              kind: contact_info[:category_code],
-              address_1: address[:location_street],
-              address_2: nil,
-              address_3: nil,
-              city: address[:location_city_name],
-              county: address[:location_county_name],
-              county_code: address[:location_county_code],
-              state: address[:location_state_us_postal_service_code],
-              zip: address[:location_postal_code],
-              country_name: nil
-            }
-          end
+        private
+
+        def address_hash(address, contact_info)
+          {
+            kind: contact_info[:category_code].downcase,
+            address_1: address[:location_street][:street_full_text],
+            address_2: nil,
+            address_3: nil,
+            city: address[:location_city_name],
+            county: address[:location_county_name],
+            county_code: address[:location_county_code],
+            state: address[:location_state_us_postal_service_code],
+            zip: address[:location_postal_code],
+            country_name: nil
+          }
         end
       end
     end
   end
-  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Layout/LineLength, Metrics/AbcSize, Metrics/ClassLength
-  
+end
+
+# File.open(Pathname.pwd.join('spec/support/atp/sample_payloads/aml.json'), "w+") do |f|
+#   f << record.to_json
+# end
