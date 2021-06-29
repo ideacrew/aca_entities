62c61118fb4783e03999a9ef6e8e81f0e9a52ecb
mdkaraman <mdkaramanov@gmail.com>
2021-06-29 15:24:45 -0400

    Added required attributes to medicaid/atp/insurance_application

--- lib/aca_entities/serializers/xml/medicaid/atp/lawful_presence_status_eligibility.rb
+++ lib/aca_entities/serializers/xml/medicaid/atp/lawful_presence_status_eligibility.rb
@@ -5,24 +5,15 @@ module AcaEntities
     module Xml
       module Medicaid
         module Atp
-<<<<<<< HEAD
-          # An eligibility of a person to receive benefits under the Affordable Care Act. 
-=======
           # mapping xml Atp::LawfulPresenceStatusEligibility
->>>>>>> refs/remotes/origin/happymapper_example_and_test
           class LawfulPresenceStatusEligibility
             include HappyMapper
 
             tag 'LawfulPresenceStatusEligibility'
             namespace 'hix-ee'
 
-<<<<<<< HEAD
             element :eligibility_indicator, Boolean, tag: 'EligibilityIndicator', namespace: "hix-ee"
             element :eligibility_basis_status_code, String, tag: 'EligibilityBasisStatusCode', namespace: "hix-ee"
-=======
-            element :status_indicator, Boolean, tag: 'StatusIndicator', namespace: "hix-core"
-            element :eligibility_basis_status_code, Boolean, tag: 'EligibilityBasisStatusCode', namespace: "hix-ee"
->>>>>>> refs/remotes/origin/happymapper_example_and_test
 
             def self.domain_to_mapper(_medicaid_eligibility)
               self.new
