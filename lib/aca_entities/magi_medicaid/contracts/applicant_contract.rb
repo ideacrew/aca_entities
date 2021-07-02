0d4e4cbb1b93350a587afe7db77c337fd25d07aa
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-07-02 09:23:43 -0400

    Refs #IapToMitcTransformer, add new attribute to Applicant and transform this to map with Mitc's is_required_to_file_taxes with tests.

--- lib/aca_entities/magi_medicaid/contracts/applicant_contract.rb
+++ lib/aca_entities/magi_medicaid/contracts/applicant_contract.rb
@@ -69,6 +69,7 @@ module AcaEntities
         # @option opts [Boolean] :is_claimed_as_dependent_by_non_applicant optional
         # @option opts [hash] :mitc_income optional
         # @option opts [Array] :mitc_relationships optional
+        # @option opts [Boolean] :mitc_is_required_to_file_taxes optional
         # @option opts [AcaEntities::MagiMedicaid::Contracts::BenchmarkPremiumContract] :benchmark_premium required
         # @option opts [Bool] :is_homeless required
         # @return [Dry::Monads::Result]
@@ -162,6 +163,7 @@ module AcaEntities
           # Set of attributes specific to MitC which helps to not have much logic in IapTo MitC Transform.
           optional(:mitc_income).hash(AcaEntities::MagiMedicaid::Mitc::Contracts::IncomeContract.params)
           optional(:mitc_relationships).array(AcaEntities::MagiMedicaid::Mitc::Contracts::RelationshipContract.params)
+          optional(:mitc_is_required_to_file_taxes).maybe(:bool)
 
         end
       end
