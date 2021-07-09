1351479ec94692498a354e965a47bd0c891f6d16
Trey Evans <lewis.r.evans@gmail.com>
2021-07-09 12:02:18 -0400

    Add secondary request encoding.

--- lib/aca_entities/serializers/xml/fdsh/ridp/operations.rb
+++ lib/aca_entities/serializers/xml/fdsh/ridp/operations.rb
@@ -2,6 +2,7 @@
 
 require_relative "operations/validate_ridp_payload_xml"
 require_relative "operations/primary_request_to_xml"
+require_relative "operations/secondary_request_to_xml"
 
 module AcaEntities
   module Serializers
