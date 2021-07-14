c4e0b7ce67debc316e580778d407d7d66560252a
Kristin Merbach <kristin@kristinmerbach.com>
2021-07-14 12:53:38 -0400

    rubocop

--- lib/aca_entities/atp/transformers/cv/family.rb
+++ lib/aca_entities/atp/transformers/cv/family.rb
@@ -78,7 +78,7 @@ module AcaEntities
                   map 'birth_date.date', 'person.person_demographics.dob', memoize: true, append_identifier: true
                   add_key 'person.person_demographics.date_of_death'
                   add_key 'person.person_demographics.dob_check'
-                  add_key 'person.person_demographics.is_incarcerated', function: ->(v) {
+                  add_key 'person.person_demographics.is_incarcerated', function: lambda { |v|
                     member_id = v.find(/record.people.(\w+)$/).map(&:item).last
                     applicants = v.resolve(:'insurance_application.insurance_applicants').item
                     applicant = applicants[:"#{member_id}"]
