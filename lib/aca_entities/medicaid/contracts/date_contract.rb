4b953eb493a417bce985fa89404eac6c6e526da7
mdkaraman <mdkaramanov@gmail.com>
2021-07-28 15:50:31 -0400

    activity date

---
+++ lib/aca_entities/medicaid/contracts/date_contract.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Medicaid
+    module Contracts
+      class DateContract < Dry::Validation::Contract
+        params do
+          optional(:date).maybe(:date)
+          optional(:date_time).maybe(:date_time)
+          optional(:year).maybe(:string)
+          optional(:year_month).maybe(:string)
+        end
+      end
+    end
+  end
+end
