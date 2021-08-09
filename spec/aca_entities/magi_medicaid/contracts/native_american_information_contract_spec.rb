1d042c6b6c43189fa86a3b8609b79f3b23cb60d7
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-08-09 15:09:19 -0400

    Refs #AmericanIndianAlaskanNative: add new attributes tribal_name & tribal_state with rspecs.

--- spec/aca_entities/magi_medicaid/contracts/native_american_information_contract_spec.rb
+++ spec/aca_entities/magi_medicaid/contracts/native_american_information_contract_spec.rb
@@ -5,7 +5,12 @@ require 'aca_entities/magi_medicaid/libraries/iap_library'
 
 RSpec.describe AcaEntities::MagiMedicaid::Contracts::NativeAmericanInformationContract,  dbclean: :after_each do
   context 'valid params' do
-    let(:input_params) { { indian_tribe_member: true, tribal_id: '123456789' } }
+    let(:input_params) do
+      { indian_tribe_member: true,
+        tribal_id: '123456789',
+        tribal_name: 'Tribal Name',
+        tribal_state: 'VA' }
+    end
 
     it 'should return success' do
       expect(subject.call(input_params)).to be_success
