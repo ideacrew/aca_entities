1dae3b2267a8ca8fc10cf929bd5866a0ad79f2a8
saimekala07 <saimekala07@gmail.com>
2021-08-09 18:49:55 -0400

    Make optional

--- lib/aca_entities/determinations/product_eligibility_determination.rb
+++ lib/aca_entities/determinations/product_eligibility_determination.rb
@@ -10,9 +10,9 @@ module AcaEntities
       attribute :is_totally_ineligible,                   Types::Bool.optional.meta(omittable: true)
       attribute :is_without_assistance,                   Types::Bool.optional.meta(omittable: true)
       attribute :is_magi_medicaid,                        Types::Bool.optional.meta(omittable: true)
-      attribute :magi_medicaid_monthly_household_income,  AcaEntities::Curreny.meta(omittable: false)
+      attribute :magi_medicaid_monthly_household_income,  AcaEntities::Curreny.optional.meta(omittable: false)
       attribute :medicaid_household_size,                 Types::Integer.optional.meta(omittable: true)
-      attribute :magi_medicaid_monthly_income_limit,      AcaEntities::Curreny.meta(omittable: false)
+      attribute :magi_medicaid_monthly_income_limit,      AcaEntities::Curreny.optional.meta(omittable: false)
       attribute :magi_as_percentage_of_fpl,               Types::Float.optional.meta(omittable: true)
       attribute :magi_medicaid_category,                  MagiMedicaid::Types::MagiMedicaidCategoryType.optional.meta(omittable: true)
     end
