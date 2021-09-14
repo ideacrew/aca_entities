04d837cab7070dfd9b33a960f43d135816bb8414
Vishal Kalletla <vishuk1201@gmail.com>
2021-09-14 10:21:22 -0400

    ref# transforms: bug fix - phone

--- lib/aca_entities/functions/phone.rb
+++ lib/aca_entities/functions/phone.rb
@@ -10,6 +10,7 @@ module AcaEntities
         return [] unless cache.resolve('family.family_members.is_primary_applicant').item == member_identifier
         contact_information = cache.resolve('attestations.application.contactInformation').item
 
+        return [] if contact_information[:primaryPhoneNumber].nil?
         primary_phone = Ffe::Transformers::Cv::Phone.transform(contact_information[:primaryPhoneNumber]&.merge!(primary: true))
         secondary_phone = Ffe::Transformers::Cv::Phone.transform(contact_information[:SecondaryPhoneNumber]&.merge!(primary: false) || [])
         [primary_phone, secondary_phone].compact
