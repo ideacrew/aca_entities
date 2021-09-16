6d3bbf10cfc9a1e57638891e1db6de8eb8243d7a
Vishal Kalletla <vishuk1201@gmail.com>
2021-09-16 14:24:18 -0400

    ref# mcr transforms: code refactor as per taxHouseHoldComposition

--- lib/aca_entities/functions/build_tax_household.rb
+++ lib/aca_entities/functions/build_tax_household.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/MethodLength, Lint/SafeNavigationChain, Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity
 module AcaEntities
   module Functions
     # taxhousehold builder
@@ -8,34 +8,39 @@ module AcaEntities
       def call(context)
         return [] unless context.find('is_applying_for_assistance')&.first&.item == true
         begin
-          @memoized_data = context
-          @primary_applicant_id = @memoized_data.resolve('family.family_members.is_primary_applicant').item
-          tax_info = @memoized_data.find(Regexp.new("computed.taxHouseholds"))&.first&.item
-          max_aptc = tax_info[:maxAPTC].present? ? (tax_info[:maxAPTC][:maxAPTCAmount] || 0.0) : 0.0
-          tax_household_annual_income = tax_info[:maxAPTC].present? ? (tax_info[:maxAPTC][:taxHouseholdAnnualIncomeAmount] || 0.0) : 0.0
-          return [] if tax_info[:taxHouseHoldComposition][:taxHouseholdMemberIdentifiers]&.first.nil?  # TODO
-          tax_household_members = tax_info[:taxHouseHoldComposition][:taxHouseholdMemberIdentifiers]&.first[:taxHouseholdMemberIdentifiers]
-          @members = []
-          tax_household_members.each do |member_id|
-            @members << {
-              family_member_reference: { family_member_hbx_id: member_id,
+          @primary_applicant_id = context.resolve('family.family_members.is_primary_applicant').item
+          tax_households = context.find(Regexp.new("computed.taxHouseholds"))
+
+          # multi tax household scenario is not devveloped for maine
+          # currently combining both multi taxhouseholds to one tax household
+          # TODO: refactor below for multiple tax households creation
+          household_member_identifiers = tax_households.collect {|obj|  obj.name.split('.').last }.flatten.compact
+          tax_composition_member_identifiers = tax_households.collect do |obj|
+            obj.item.dig(:taxHouseHoldComposition, :taxHouseholdMemberIdentifiers, 0, :taxHouseholdMemberIdentifiers)
+          end.flatten.compact
+
+          tax_household_members = household_member_identifiers.collect do |household_member_identifier|
+            {
+              family_member_reference: { family_member_hbx_id: household_member_identifier,
                                          first_name: '',
                                          last_name: '',
                                          person_hbx_id: '',
-                                         is_primary_family_member: (@primary_applicant_id == member_id) },
-              product_eligibility_determination: { is_ia_eligible: true },
-              is_subscriber: @primary_applicant_id == member_id,
+                                         is_primary_family_member: (@primary_applicant_id == household_member_identifier) },
+              product_eligibility_determination: { is_ia_eligible: tax_composition_member_identifiers.include?(household_member_identifier) },
+              is_subscriber: @primary_applicant_id == household_member_identifier,
               reason: ""
             }
           end
 
-          # TODO: received max_aptc is not in cents
+          max_aptc = tax_households.sum {|b|  b.item.dig(:maxAPTC, :maxAPTCAmount).to_f}
+          tax_household_annual_income = tax_households.sum { |b| b.item.dig(:maxAPTC, :taxHouseholdAnnualIncomeAmount).to_f }
+
           [hbx_id: "1234",
            allocated_aptc: { cents: 0, currency_iso: "USD" },
            is_eligibility_determined: true,
            start_date: Date.new(2021, 1, 1), # default value
            end_date: Date.new(2021, 12, 31), # default value
-           tax_household_members: @members,
+           tax_household_members: tax_household_members&.flatten&.compact,
            eligibility_determinations: [{
              max_aptc: { cents: max_aptc, currency_iso: "USD" },
              csr_percent_as_integer: 0, # default value
@@ -51,4 +56,4 @@ module AcaEntities
     end
   end
 end
-# rubocop:enable Metrics/MethodLength, Lint/SafeNavigationChain, Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity
