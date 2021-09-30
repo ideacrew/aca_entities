eeb69bc35b3070eb38f11a656ff6329c2720f0f9
saimekala07 <saimekala07@gmail.com>
2021-09-30 00:19:59 -0400

    Fix broker mapping

--- lib/aca_entities/functions/broker_account_builder.rb
+++ lib/aca_entities/functions/broker_account_builder.rb
@@ -24,6 +24,40 @@ module AcaEntities
       # CERTIFIED_APPLICATION_COUNSELOR
       # NAVIGATOR
       # IN_PERSON_ASSISTANT_PERSONNEL
+      BrokerNPNMapping = {"7383343"=>"10609371",
+                          "17008816"=>"17008817",
+                          "117367"=>"17311367",
+                          "1735141"=>"17325141",
+                          "260513"=>"17783181",
+                          "3604039"=>"3684039",
+                          "3685342"=>"3685342",
+                          "711"=>"3688932",
+                          "867302"=>"8673207",
+                          "1795526"=>"17955267",
+                          "36844183"=>"3684183",
+                          "36812194"=>"3682194",
+                          "1926613"=>"19263613",
+                          "87818504"=>"8718504",
+                          "4692969"=>"4632969",
+                          "368188"=>"3681888",
+                          "36801451"=>"3681451",
+                          "8375570"=>"8375770",
+                          "10564809"=>"10568409",
+                          "9544749"=>"9544739",
+                          "1111111"=>"3351659",
+                          "3551659"=>"3351659",
+                          "8591025"=>"8951425",
+                          "1068793"=>"10687973",
+                          "164112"=>"16411232",
+                          "245892"=>"15953846",
+                          "1877628"=>"18776268",
+                          "2077979899"=>"7979899",
+                          "231746"=>"2315746",
+                          "155883573"=>"15883573",
+                          "114661908"=>"14661908",
+                          "33513117"=>"3351317",
+                          "3371317"=>"3351317",
+                          "2391255"=>"16440994"}
 
       def call(cache)
         broker_accounts = cache.resolve('attestations.application').item[:applicationAssistors]
@@ -43,7 +77,7 @@ module AcaEntities
 
       def broker_role_reference_hash(account)
         {
-          npn: account[:assistorNationalProducerNumber].to_s,
+          npn: BrokerNPNMapping[account[:assistorNationalProducerNumber].to_s] || account[:assistorNationalProducerNumber].to_s,
           person_reference: person_reference_hash(account),
           broker_agency_reference: default_agency_hash
         }
