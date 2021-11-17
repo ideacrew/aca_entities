324a38a8fe4328257c287ba81234154a2ddd6a03
mdkaraman <mdkaramanov@gmail.com>
2021-11-17 16:26:29 -0500

    add coverage indicator field to insurance applicant

--- lib/aca_entities/medicaid/atp/insurance_applicant.rb
+++ lib/aca_entities/medicaid/atp/insurance_applicant.rb
@@ -12,6 +12,7 @@ module AcaEntities
         attribute :foster_care_state, Types::String.optional.meta(omittable: true)
         attribute :had_medicaid_during_foster_care_indicator, Types::Bool.optional.meta(omittable: true)
         attribute :blindness_or_disability_indicator, Types::Bool.optional.meta(omittable: true)
+        attribute :coverage_during_previous_six_months_indicator, Types::Bool.optional.meta(omittable: true)
         attribute :lawful_presence_status, InsuranceApplicantLawfulPresenceStatus.optional.meta(omittable: true)
         attribute :long_term_care_indicator, Types::Bool.optional.meta(omittable: true)
         attribute :medicaid_magi_eligibilities, Types::Array.of(MedicaidMagiEligibility).optional.meta(omittable: true)
