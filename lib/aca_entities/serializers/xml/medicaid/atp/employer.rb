793fbf5a96c8bce6da1a6784501909b73adb5579
mdkaraman <mdkaramanov@gmail.com>
2021-11-17 11:48:50 -0500

    add organization augmentation serializer

--- lib/aca_entities/serializers/xml/medicaid/atp/employer.rb
+++ lib/aca_entities/serializers/xml/medicaid/atp/employer.rb
@@ -15,6 +15,7 @@ module AcaEntities
             attribute :id, String, namespace: "niem-s"
             element :category_text, String, tag: 'OrganizationName', namespace: "nc"
             has_one :organization_primary_contact_information, OrganizationPrimaryContactInformation
+            has_one :organization_augmentation, OrganizationAugmentation
 
             def self.domain_to_mapper(employer)
               mapper = self.new
@@ -22,6 +23,7 @@ module AcaEntities
               mapper.category_text = employer.category_text
               mapper.organization_primary_contact_information =
                 OrganizationPrimaryContactInformation.domain_to_mapper(employer.organization_primary_contact_information)
+              mapper.organization_augmentation = OrganizationAugmentation.domain_to_mapper(employer.organization_augmentation)
               mapper
             end
 
@@ -29,7 +31,8 @@ module AcaEntities
               {
                 id: id,
                 category_text: category_text,
-                organization_primary_contact_information: organization_primary_contact_information&.to_hash
+                organization_primary_contact_information: organization_primary_contact_information&.to_hash,
+                organization_augmentation: organization_augmentation&.to_hash
               }
             end
           end
