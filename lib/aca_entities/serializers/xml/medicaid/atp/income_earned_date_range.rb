ec4a011ceb7b123cbd7ced8f0f40b7638cc6ccab
mdkaraman <mdkaramanov@gmail.com>
2021-11-19 14:50:52 -0500

    fix income earned date range mapping

--- lib/aca_entities/serializers/xml/medicaid/atp/income_earned_date_range.rb
+++ lib/aca_entities/serializers/xml/medicaid/atp/income_earned_date_range.rb
@@ -17,8 +17,8 @@ module AcaEntities
 
             def self.domain_to_mapper(income_earned_date_range)
               mapper = self.new
-              mapper.start_date = StartDate.domain_to_mapper(income_earned_date_range.start_date)
-              mapper.end_date = EndDate.domain_to_mapper(income_earned_date_range.end_date)
+              mapper.start_date = StartDate.domain_to_mapper(income_earned_date_range.start_date) if income_earned_date_range.start_date.date
+              mapper.end_date = EndDate.domain_to_mapper(income_earned_date_range.end_date) if income_earned_date_range.end_date.date
               mapper
             end
 
