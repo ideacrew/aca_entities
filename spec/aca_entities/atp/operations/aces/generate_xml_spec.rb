50587859b3172245bc12f14e186dc77e058c4af1
Kristin Merbach <kristin@kristinmerbach.com>
2021-11-24 08:38:46 -0500

    fix business validation issue with medicaid household

--- spec/aca_entities/atp/operations/aces/generate_xml_spec.rb
+++ spec/aca_entities/atp/operations/aces/generate_xml_spec.rb
@@ -6,17 +6,34 @@ require 'aca_entities/atp/operations/aces/generate_xml'
 require 'aca_entities/serializers/xml/medicaid/atp'
 require 'aca_entities/serializers/xml/medicaid/atp/account_transfer_request'
 require 'pry'
+require 'open3'
 
 RSpec.describe AcaEntities::Atp::Operations::Aces::GenerateXml  do
   describe 'When a valid json file is passed' do
 
     let(:payload) { File.read(Pathname.pwd.join("spec/support/atp/sample_payloads/complex_b.json")) }
 
+    let(:schematron_location) do
+      loc = File.join(
+        File.dirname(__FILE__),
+        "..", "..", "..", "..",
+        "reference", "xml", "atp"
+      )
+      File.expand_path(loc)
+    end
+
+    let(:business_error_ns) do
+      {
+        svrl: "http://purl.oclc.org/dsdl/svrl"
+      }
+    end
+
     it 'should parse and then transform when transform_mode set to batch' do
       result = described_class.new.call(payload)
       _example_output_xml = File.read(Pathname.pwd.join('spec/support/atp/sample_payloads/simple_L_transformed_payload.xml'))
-      puts result
+
       expect(result.success?).to be_truthy
     end
+
   end
 end
