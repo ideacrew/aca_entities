49e200ebd7371f3110eb86023e0dc2bfe5a695d5
mdkaraman <mdkaramanov@gmail.com>
2021-11-30 10:02:23 -0500

    add person augmentation to serializer

--- lib/aca_entities/serializers/xml/medicaid/atp/role_played_by_person.rb
+++ lib/aca_entities/serializers/xml/medicaid/atp/role_played_by_person.rb
@@ -15,12 +15,15 @@ module AcaEntities
             has_one :person_name, PersonName
             has_one :birth_date, PersonBirthDate
             element :sex, String, tag: "PersonSexText", namespace: "nc"
+            has_one :person_augmentation, PersonAugmentation
+
 
             def self.domain_to_mapper(role)
               mapper = self.new
               mapper.person_name = PersonName.domain_to_mapper(role.person_name)
               mapper.birth_date = PersonName.domain_to_mapper(role.birth_date)
               mapper.sex = role.sex
+              mapper.person_augmentation = PersonAugmentation.domain_to_mapper(person.person_augmentation)
               mapper
             end
 
@@ -29,6 +32,7 @@ module AcaEntities
                 person_name: person_name&.to_hash,
                 sex: sex,
                 birth_date: birth_date&.to_hash
+                augementation: person_augmentation&.to_hash
               }
             end
           end
