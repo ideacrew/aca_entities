ef3801c5f87773f0deb3086018a0c9ed1c308b96
mdkaraman <mdkaramanov@gmail.com>
2021-11-30 10:52:46 -0500

    add person identification

---
+++ lib/aca_entities/serializers/xml/medicaid/atp/person_identification.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Serializers
+    module Xml
+      module Medicaid
+        module Atp
+          # An identification assigned to an individual.
+          class PersonIdentification
+            include HappyMapper
+
+            tag 'PersonIdentification'
+            namespace 'hix-core'
+
+            element :identification_id, String, tag: "IdentificationID", namespace: "nc"
+            element :identification_category_text, String, tag: "IdentificationCategoryText", namespace: "nc"
+            element :identification_jurisdiction, String, tag: "IdentificationJurisdictionISO3166Alpha3Code", namespace: "nc"
+
+            def self.domain_to_mapper(identification)
+              mapper = self.new
+              mapper.identification_id = identification.identification_id
+              mapper
+            end
+
+            def to_hash
+              {
+                identification_id: identification_id,
+                identification_category_text: identification_category_text,
+                identification_jurisdiction: identification_jurisdiction
+              }
+            end
+          end
+        end
+      end
+    end
+  end
+end
