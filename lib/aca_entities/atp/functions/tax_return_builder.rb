a51b7723a8edb2c9e12635c0f84718fc90e74307
mdkaraman <mdkaramanov@gmail.com>
2021-12-01 16:20:11 -0500

    add claimed by indicator to dependent hash

--- lib/aca_entities/atp/functions/tax_return_builder.rb
+++ lib/aca_entities/atp/functions/tax_return_builder.rb
@@ -10,26 +10,26 @@ module AcaEntities
         # rubocop:disable Metrics/MethodLength
         def call(cache)
           @memoized_data = cache
-          applicants_hash = @memoized_data.resolve('family.magi_medicaid_applications.applicants').item
-          member_id = @memoized_data.find(Regexp.new('is_primary_applicant.*')).select {|a|  a.item == true}.first.name.split('.').last
-          person_relationships = applicants_hash[member_id.to_sym][:mitc_relationships]
+          @applicants_hash = @memoized_data.resolve('family.magi_medicaid_applications.applicants').item
+          @member_id = @memoized_data.find(Regexp.new('is_primary_applicant.*')).select {|a|  a.item == true}.first.name.split('.').last
+          @person_relationships = @applicants_hash[@member_id.to_sym][:mitc_relationships]
           tax_households = @memoized_data.resolve('family.magi_medicaid_applications.tax_households').item
 
           return unless tax_households
           tax_households.values.each_with_object([]) do |household, collect|
             members = household[:tax_household_members].map {|m| m.dig(:applicant_reference, :person_hbx_id)}
 
-            primary_tax_filer = find_primary_tax_filer(members, applicants_hash).first.to_s
+            primary_tax_filer = find_primary_tax_filer(members).first.to_s
             primary_role_reference = { ref: "pe#{primary_tax_filer}" }
             primary_hash = { role_reference: primary_role_reference } if primary_tax_filer.present?
 
-            spouse_tax_filer = find_spouse_tax_filer(members, person_relationships, primary_tax_filer)
+            spouse_tax_filer = find_spouse_tax_filer(members)
             spouse_role_reference = { ref: "pe#{spouse_tax_filer}" }
             spouse_hash = { role_reference: spouse_role_reference } if spouse_tax_filer.present?
-            binding.pry
-            dependents = find_dependents(members, applicants_hash).map(&:to_s)
+
+            dependents = find_dependents(members).map(&:to_s)
             dependents_hash = dependents.map do |id|
-              claimed_by_custodial_parent = is_claimed_by_custodial_parent(id, applicants_hash, person_relationships)
+              claimed_by_custodial_parent = claimed_by_custodial_parent?(id)
               { role_reference: { ref: "pe#{id}" },
                 claimed_by_custodial_parent_indicator: claimed_by_custodial_parent }
             end
@@ -64,29 +64,28 @@ module AcaEntities
           income.to_s.insert(-3, ".")
         end
 
-        def find_primary_tax_filer(members, applicants_hash)
-          head_of_household = members.map(&:to_sym) & applicants_hash.select {|_k, v| v[:is_filing_as_head_of_household] == true}.keys
+        def find_primary_tax_filer(members)
+          head_of_household = members.map(&:to_sym) & @applicants_hash.select {|_k, v| v[:is_filing_as_head_of_household] == true}.keys
           return head_of_household unless head_of_household.empty?
 
-          members.map(&:to_sym) & applicants_hash.select {|_k, v| v[:is_primary_applicant].present?}.keys
+          members.map(&:to_sym) & @applicants_hash.select {|_k, v| v[:is_primary_applicant].present?}.keys
         end
 
-        def find_spouse_tax_filer(members, person_relationships, _member_id)
-          person_relationships.select {|h| h[:relationship_code] == "02"}.each do |rel|
+        def find_spouse_tax_filer(members)
+          @person_relationships.select {|h| h[:relationship_code] == "02"}.each do |rel|
             spouse = [rel[:other_id]] & members
             return spouse.first unless spouse.empty?
           end
         end
 
-        def find_dependents(members, applicants_hash)
-          members.map(&:to_sym) & applicants_hash.select {|_k, v| v[:is_claimed_as_tax_dependent] == true}.keys
+        def find_dependents(members)
+          members.map(&:to_sym) & @applicants_hash.select {|_k, v| v[:is_claimed_as_tax_dependent] == true}.keys
         end
 
-        def is_claimed_by_custodial_parent(id, applicants_hash, person_relationships)          
-          claimed_by = applicants_hash[id.to_sym].dig(:claimed_as_tax_dependent_by, :person_hbx_id)
-          person_relationships.select {|h| h[:relationship_code] == "03"}.each do |rel|
-            binding.pry
-            return true if rel[:other_id] == claimed_by
+        def claimed_by_custodial_parent?(id)
+          claimed_by = @applicants_hash[id.to_sym].dig(:claimed_as_tax_dependent_by, :person_hbx_id)
+          @person_relationships.select {|h| h[:relationship_code] == "03"}.each do |rel|
+            return true if rel[:other_id] == id && @member_id == claimed_by
           end
           false
         end
--- lib/aca_entities/atp/operations/aces/generate_xml.rb
+++ lib/aca_entities/atp/operations/aces/generate_xml.rb
@@ -19,8 +19,6 @@ module AcaEntities
           include Dry::Monads[:result, :do, :try]
 
           def call(payload)
-            binding.pry
-
             valid_payload = yield validate(payload)
             aces_payload = yield to_aces(valid_payload)
             valid_aces_payload = yield validate_aces(aces_payload)
