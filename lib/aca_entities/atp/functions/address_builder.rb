0d07ddb395877754b49e5d1f36ba24446b207351
mdkaraman <mdkaramanov@gmail.com>
2021-12-10 15:36:14 -0500

    refactor zip formatting

--- lib/aca_entities/atp/functions/address_builder.rb
+++ lib/aca_entities/atp/functions/address_builder.rb
@@ -21,8 +21,7 @@ module AcaEntities
         private
 
         def address_hash(address, contact_info)
-          zip = address[:location_postal_code]
-          zip = zip[0..4] unless zip.nil? || zip.empty? # only store 5-digit zip code
+          zip = address[:location_postal_code][0..4] unless address[:location_postal_code].blank?  # only store 5-digit zip code
           {
             kind: contact_info[:category_code].downcase,
             address_1: address[:location_street][:street_full_text],
