b9089ab57e4a8a80a618caec8c74bfa799ae40b9
ipublic <dann@ideacrew.com>
2021-12-12 18:57:59 -0500

    WIP on determination_contract and spec

--- lib/aca_entities/eligibilities/contracts/determination_contract.rb
+++ lib/aca_entities/eligibilities/contracts/determination_contract.rb
@@ -5,11 +5,31 @@ module AcaEntities
     module Contracts
       # contract for EvidenceContract
       class DeterminationContract < Dry::Validation::Contract
-        params do
-          required(:key).filled(:string)
-          required(:status).filled(AcaEntities::Eligibilities::Types::DeterminationStateKind)
-          optional(:earliest_due_date).filled(:date)
-          optional(:determined_at).filled(:date_time)
+        EligibilitySchema =
+          Dry::Schema.Params do
+            required(:status).filled(AcaEntities::Eligibilities::Types::DeterminationStateKind)
+            required(:earliest_due_date).filled(:date)
+            required(:determined_at).filled(:date_time)
+            required(:evidence_states).value(:hash)
+          end
+
+        rule(:evidence_states).each do
+          values
+            .to_h
+            .slice(:evidence_states)
+            .each_pair do |evidence_state_key, evidence_state_val|
+              result = EligibilitySchema.call(evidence_state_val)
+              result.errors.to_h.each { |path, message| key([:determinations, *path]).failure(message) } if result.failure?
+            end
+        end
+
+        params { required(:determinations).value(:hash) }
+
+        rule(:determinations).each do
+          values.to_h[:determinations].each_pair do |eligibility_key, eligibility_val|
+            result = EligibilitySchema.call(eligibility_val)
+            result.errors.to_h.each { |path, message| key([:determinations, *path]).failure(message) } if result.failure?
+          end
         end
       end
     end
--- lib/aca_entities/eligibilities/evidence_item.rb
+++ lib/aca_entities/eligibilities/evidence_item.rb
@@ -3,13 +3,41 @@
 module AcaEntities
   # All types Eligibilities
   module Eligibilities
-    # Reference to an class or other object type instance
+    # A configuration setting that defines the Evidnece relationship
+    # between two objects.  The SubjectRef is the entity object
+    # which being described and the EvidenceRef is the entity
+    # object that manages a fact about the SubjectRef to be verified.
+    #
+    # For example, a SubjectRef may be a FamilyMember and an EvidenceRef
+    # may be a ConsumerRole that maintains the Verification of Lawful
+    # Presence (VLP) for that individual.
     class EvidenceItem < Dry::Struct
       attribute :id, Types::String.meta(ommittable: true)
+
+      # @!attribute [r] key
+      # An unambiguous reference to this EvidenceItem
+      # @return [String]
       attribute :key, Types::Coercible::String.meta(omittable: false)
+
+      # @!attribute [r] title
+      # A name given to the resource by which the resource is formally known
+      # @return [String]
       attribute :title, Types::String.meta(omittable: true)
-      attribute :description, Types::String.optional.meta(omittable: true)
+
+      # @!attribute [r] description
+      # An optional account of the content of this resource
+      # @return [String]
+      attribute :description, Types::String.optional.meta(ommitable: true)
+
+      # @!attribute [r] subject_ref
+      # A class or object described by the {evidence_ref}
+      # @return [String]
       attribute :subject_ref, Types::String.optional.meta(omittable: false)
+
+      # @!attribute [r] evidence_ref
+      # A class or object that maintains the state for a fact
+      # about the {subject_ref}
+      # @return [String]
       attribute :evidence_ref, Types::String.optional.meta(omittable: false)
     end
   end
