ae61e21e0e1f55ea4698039b3d088c959ca7080d
ipublic <dann@ideacrew.com>
2021-12-13 18:19:41 -0500

    Fix class declaration

--- lib/aca_entities/eligibilities/eligibility.rb
+++ lib/aca_entities/eligibilities/eligibility.rb
@@ -22,20 +22,57 @@ module AcaEntities
     require_relative 'subject'
     require_relative 'eligibility_snapshot'
 
-    attribute :status, AcaEntities::Eligibilities::Types::DeterminationStateKind.meta(omittable: false)
-    attribute :earliest_due_date, Types::Date.meta(omittable: false)
-    attribute :determined_at, Types::DateTime.meta(omittable: false)
-    attribute :evidence_states, AcaEntities::Eligibilities::EvidenceStates.meta(omittable: false)
+    # A granted permission or benefit
+    class Eligibility < Dry::Struct
+      attribute :status, AcaEntities::Eligibilities::Types::DeterminationStateKind.meta(omittable: false)
+      attribute :earliest_due_date, Types::Date.meta(omittable: false)
+      attribute :determined_at, Types::DateTime.meta(omittable: false)
+      attribute :evidence_states, AcaEntities::Eligibilities::EvidenceState.meta(omittable: false)
 
-    def status
-      evidence_states.any?
+      def status
+        evidence_states.any?
+      end
+
+      def earliest_evidence_due_date
+        evidence_states
+      end
+
+      def determined_at; end
+
+      # class Family
+      #   embeds_many :eligibility_snapshots
+      # end
+
+      # class EligibilitySnapshot
+      #   attribute :enrollment_period # OE or SEP
+
+      #   attribute :eligibility_items
+      #   attribute :subjects
+      # end
+
+      # class EligibilitySnapshotHistory
+      #   belongs_to :family
+      #   has_many :eligibility_snapshots
+
+      #   def self.eligibilities_on(date)
+      #     []
+      #   end
+      # end
+
+      # This is an Operation with corresponding Event
+      # module Operations
+      #   module Eligibilities
+      #     class CreateEligibilitySnapshot
+      #       params family, options = { event: event, evidences: [:non_esi] }
+      #       def call(params)
+      #         values = yield validate(params)
+      #         eligibilities = yield(values)
+
+      #         income_eligibilities
+      #       end
+      #     end
+      #   end
+      # end
     end
-
-    def earliest_evidence_due_date
-      evidence_states
-    end
-
-    def determined_at; end
-
   end
 end
