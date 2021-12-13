f5cb031215c598e07b5b5902595378c0318ba777
ipublic <dann@ideacrew.com>
2021-12-13 12:05:36 -0500

    Add rules for is_satisfied/due_on values

--- lib/aca_entities/eligibilities/contracts/evidence_state_contract.rb
+++ lib/aca_entities/eligibilities/contracts/evidence_state_contract.rb
@@ -10,8 +10,13 @@ module AcaEntities
           required(:state).filled(AcaEntities::Eligibilities::Types::EvidenceStateKind)
           required(:is_satisfied).filled(:bool)
           required(:verification_outstanding).filled(:bool)
-          required(:due_on).filled(:date)
+          required(:due_on).maybe(:date)
           required(:visited_at).filled(:date_time)
+        end
+
+        rule(:is_satisfied, :due_on) do
+          key.failure('when is_satisfied is true, due_on must be nil') if (values[:is_satisfied] && !values[:due_on].nil?)
+          key.failure('when is_satisfied is false, due_on must be present') if (!values[:is_satisfied] && values[:due_on].nil?)
         end
       end
     end
