1aa2acc978f496d91d08d33a1a5544bb3b8851c1
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-12-29 13:17:39 -0500

    180551173 - reverts changes made in branch: 180551173_cv3_validation_non_applicant

--- lib/aca_entities/magi_medicaid/contracts/attestation_contract.rb
+++ lib/aca_entities/magi_medicaid/contracts/attestation_contract.rb
@@ -8,14 +8,14 @@ module AcaEntities
         # @!method call(opts)
         # @param [Hash] opts the parameters to validate using this contract
         # @option opts [Boolean] :is_incarcerated optional
-        # @option opts [Boolean] :is_self_attested_disabled optional
-        # @option opts [Boolean] :is_self_attested_blind optional
+        # @option opts [Boolean] :is_self_attested_disabled required
+        # @option opts [Boolean] :is_self_attested_blind required
         # @option opts [Boolean] :is_self_attested_long_term_care optional
         # @return [Dry::Monads::Result]
         params do
           optional(:is_incarcerated).maybe(:bool)
-          optional(:is_self_attested_disabled).maybe(:bool)
-          optional(:is_self_attested_blind).maybe(:bool)
+          required(:is_self_attested_disabled).filled(:bool)
+          required(:is_self_attested_blind).filled(:bool)
           optional(:is_self_attested_long_term_care).maybe(:bool)
         end
       end
