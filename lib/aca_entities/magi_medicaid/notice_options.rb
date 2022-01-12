c87f4e7b90f72b43d7c94acc623e40b814f2a2ad
Battula <vrbattula@outlook.com>
2022-01-12 17:51:38 -0500

    add paper notification field to notice options

--- lib/aca_entities/magi_medicaid/notice_options.rb
+++ lib/aca_entities/magi_medicaid/notice_options.rb
@@ -6,6 +6,8 @@ module AcaEntities
     class NoticeOptions < Dry::Struct
       attribute :send_eligibility_notices, Types::Bool.meta(omittable: false)
       attribute :send_open_enrollment_notices, Types::Bool.meta(omittable: false)
+
+      attribute :paper_notification, Types::Bool.optional.meta(omittable: true)
     end
   end
 end
---
+++ spec/aca_entities/magi_medicaid/notice_options_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'aca_entities/magi_medicaid/libraries/iap_library'
+
+RSpec.describe ::AcaEntities::MagiMedicaid::NoticeOptions, dbclean: :after_each do
+
+  describe 'with valid arguments' do
+    let(:input_params) do
+      { send_eligibility_notices: true, send_open_enrollment_notices: false, paper_notification: false }
+    end
+
+    before do
+      notice_options = AcaEntities::MagiMedicaid::Contracts::NoticeOptionsContract.new.call(input_params).to_h
+      @result = described_class.new(notice_options)
+    end
+
+    it 'should return NoticeOptions entity object' do
+      expect(@result).to be_a(described_class)
+    end
+
+    it 'should return all keys of NoticeOptions' do
+      expect(@result.to_h.keys).to eq(input_params.keys)
+    end
+  end
+end
