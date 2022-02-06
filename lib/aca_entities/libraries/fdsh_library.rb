ebe0d1e7833ede693b20d86d340a9c31ce3bf629
ipublic <dann@ideacrew.com>
2022-02-06 16:01:30 -0500

    Improve documentation

--- lib/aca_entities/libraries/fdsh_library.rb
+++ lib/aca_entities/libraries/fdsh_library.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Include the baseline entities and types of fdsh
+# CMS FDSH service interface library
 module AcaEntities
   module Libraries
     module FdshLibrary
