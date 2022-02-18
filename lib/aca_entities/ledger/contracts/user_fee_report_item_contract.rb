fb0623d900dfe2f518d3cd757104b9456ca1f6f0
ipublic <dann@ideacrew.com>
2022-02-17 19:13:07 -0500

    Fix attributes

--- lib/aca_entities/ledger/contracts/user_fee_report_item_contract.rb
+++ lib/aca_entities/ledger/contracts/user_fee_report_item_contract.rb
@@ -6,10 +6,10 @@ module AcaEntities
       # Schema and validation rules for {AcaEntities::Ledger::UserFeeReportItem}
       class UserFeeReportItemContract < Contract
         params do
-          required(:sequence_value).value(:integer)
-          required(:quantity).filled(:integer)
+          # required(:sequence_value).value(:integer)
           required(:hios_id).value(:string)
           required(:hbx_subscriber_id).value(:string)
+          required(:hbx_member_id).value(:string)
           required(:insurer_subscriber_id).value(:string)
           required(:last_name).value(:string)
           required(:first_name).value(:string)
@@ -18,7 +18,7 @@ module AcaEntities
           required(:marketplace_segment_id).value(:string)
           required(:insurer_policy_id).value(:string)
           required(:policy_aptc_amount).value(:float)
-          required(:member_premium).value(:float)
+          required(:member_premium_amount).value(:float)
           required(:billing_start_on).value(:date)
           required(:billing_end_on).value(:date)
           required(:fee_kind).value(AcaEntities::Ledger::Types::UserFeeReportItemKind)
