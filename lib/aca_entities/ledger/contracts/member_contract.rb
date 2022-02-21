c05038181cb61b277b6ca11da011c13128cc1e07
ipublic <dann@ideacrew.com>
2022-02-21 12:36:21 -0500

    Add subscriber_hbx_id and optional attributes Add gender type coercion to cotract

--- lib/aca_entities/ledger/contracts/member_contract.rb
+++ lib/aca_entities/ledger/contracts/member_contract.rb
@@ -9,10 +9,31 @@ module AcaEntities
           optional(:id).value(:string)
           required(:hbx_id).filled(:string)
           optional(:insurer_assigned_id).maybe(:string)
+          required(:subscriber_hbx_id).filled(:string)
+          optional(:insurer_assigned_subscriber_id).filled(:string)
           required(:person_name).filled(AcaEntities::Contracts::People::PersonNameContract.params)
+          optional(:ssn).maybe(:string)
+          optional(:dob).maybe(:date)
+          optional(:gender).maybe(AcaEntities::Types::GenderKind)
           optional(:tax_household_id).maybe(:string)
+          optional(:relationshihp_code).maybe(:string)
           optional(:is_tobacco_user).maybe(:bool)
           optional(:timestamps).maybe(AcaEntities::Contracts::TimeStampContract.params)
+
+          # Coerce common alternative gender values to GenderKind type before validating
+          before(:value_coercer) do |result|
+            parms = result.to_h
+
+            if parms.key?(:gender) && !parms[:gender].nil?
+              # require 'pry'
+              # binding.pry
+              gender_val = 'Female' if %w[Female f female girl woman].include? parms[:gender]
+              gender_val = 'Male' if %w[Male m male boy man].include? parms[:gender]
+              gender_val ||= parms[:gender]
+
+              result.to_h.merge!({ gender: gender_val }) if %w[Female Male].include? gender_val
+            end
+          end
         end
       end
     end
--- spec/aca_entities/ledger/contracts/member_contract_spec.rb
+++ spec/aca_entities/ledger/contracts/member_contract_spec.rb
@@ -6,17 +6,32 @@ RSpec.describe AcaEntities::Ledger::Contracts::MemberContract do
   subject { described_class.new }
 
   let(:id) { '12345' }
+  let(:subscriber_hbx_id) { '987654' }
   let(:hbx_id) { '987654' }
   let(:insurer_assigned_id) { '555555' }
+  let(:insurer_assigned_subscriber_id) { '555555' }
   let(:person_name) { { first_name: 'George', last_name: 'Jetson' } }
   let(:is_tobacco_user) { false }
+
+  let(:ssn) { '777665432' }
+  let(:dob) { Date.new(1988, 3, 15) }
+  let(:gender) { 'm' }
 
   let(:moment) { DateTime.now }
   let(:timestamps) { { created_at: moment, modified_at: moment } }
 
-  let(:required_params) { { hbx_id: hbx_id, person_name: person_name } }
+  let(:required_params) { { hbx_id: hbx_id, subscriber_hbx_id: subscriber_hbx_id, person_name: person_name } }
   let(:optional_params) do
-    { id: id, is_tobacco_user: is_tobacco_user, insurer_assigned_id: insurer_assigned_id, timestamps: timestamps }
+    {
+      id: id,
+      ssn: ssn,
+      dob: dob,
+      gender: gender,
+      is_tobacco_user: is_tobacco_user,
+      insurer_assigned_id: insurer_assigned_id,
+      insurer_assigned_subscriber_id: insurer_assigned_subscriber_id,
+      timestamps: timestamps
+    }
   end
 
   let(:all_params) { required_params.merge(optional_params) }
@@ -31,16 +46,35 @@ RSpec.describe AcaEntities::Ledger::Contracts::MemberContract do
     end
 
     context 'Calling the contract with all params' do
+      let(:all_params_with_coerced_gender) { all_params.merge!(gender: 'Male') }
       it 'should pass validation' do
         result = subject.call(all_params)
         expect(result.success?).to be_truthy
-        expect(result.to_h).to eq all_params
+        expect(result.to_h).to eq all_params_with_coerced_gender
       end
     end
   end
 
+  context 'Calling the contract with alternate gender values' do
+    let(:alternative_gender) { 'girl' }
+    let(:female_gender) { 'Female' }
+    let(:invalid_gender) { 'clown' }
+    let(:gender_error) { 'must be one of: Male, Female' }
+
+    it 'should coerce valid alternative gender to expected kind' do
+      result = subject.call(all_params.merge!(gender: alternative_gender))
+      expect(result[:gender]).to eq female_gender
+      expect(result.errors[:gender]).to be_nil
+    end
+
+    it 'should fail to coerce invalid alternative gender to expected kind and return error' do
+      result = subject.call(gender: invalid_gender)
+      expect(result.errors[:gender].first).to eq gender_error
+    end
+  end
+
   context 'Calling the contract with no params' do
-    let(:error_message) { { hbx_id: ['is missing'], person_name: ['is missing'] } }
+    let(:error_message) { { hbx_id: ['is missing'], subscriber_hbx_id: ['is missing'], person_name: ['is missing'] } }
     it 'should pass validation' do
       result = subject.call({})
       expect(result.failure?).to be_truthy
