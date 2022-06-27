462a0c68eb6c9cb3da42e31f2a4e0b68a72e42ee
mdkaraman <mdkaramanov@gmail.com>
2022-06-27 14:16:14 -0400

    ssp insurance policy entity

---
+++ lib/aca_entities/medicaid/ios/ssp_insurance_policy__c.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Medicaid
+    module Ios
+      # entity for SSP_InsurancePolicy__c
+      class SspInsurancePolicyC < Dry::Struct
+        attribute :DCId__c, Types::Integer.optional.meta(omittable: true)
+        attribute :Is_this_an_empl__c,  Types::String.meta(omittable: false)  # what data type is this?? bool v string
+        attribute :InsuranceCompanyName__c, Types::String.optional.meta(omittable: true)
+        attribute :InsuranceGroupNumber__c, Types::String.optional.meta(omittable: true)
+        attribute :InsurancePolicyNumber__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalAddressLine1__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalAddressLine2__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalCity__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalCountryCode__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalStateCode__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalZipCode4__c, Types::String.optional.meta(omittable: true)
+        attribute :PhysicalZipCode5__c, Types::String.optional.meta(omittable: true)
+        attribute :PlanName__c, Types::String.optional.meta(omittable: true)
+        attribute :PolicyBeginDate__c, Types::String.optional.meta(omittable: true) # does "format": "date" imply validation?
+        attribute :TypeOfCoverageCode__c, Types::String.optional.meta(omittable: true)
+        attribute :IndividualId__c, Types::Integer.optional.meta(omittable: true)
+        attribute :EnrollmentTierLevel__c, Types::String.optional.meta(omittable: true)
+        attribute :IsPolicyUpdated__c, Types::Boolean.optional.meta(omittable: true)
+        attribute :IsEnrolledInInsurance__c, Types::Boolean.optional.meta(omittable: true)
+        attribute :Id, Types::String.optional.meta(omittable: true)
+        attribute :IsDeleted__c, Types::Boolean.optional.meta(omittable: true)
+        attribute :DCEmploymentDetailId__c, Types::Integer.optional.meta(omittable: true)
+        attribute :EmployerName__c, Types::String.optional.meta(omittable: true)
+        attribute :IsHealthCareCovPolicyHolderOutSideCase__c, Types::Boolean.optional.meta(omittable: true)
+      end
+    end
+  end
+end
