8129aa77f81b6c241e69d22f84adc4529ecb1da0
mdkaraman <mdkaramanov@gmail.com>
2022-07-05 14:47:16 -0400

    set up contact builder spec and class

---
+++ lib/aca_entities/medicaid/ios/functions/contact_builder.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module AcaEntities
+  module Medicaid
+    module Ios
+      module Functions
+        # build contact for IOS transform
+        class ContactBuilder
+          def call(cache)
+            @memoized_data = cache
+            # TO DO
+            # loop through ??
+            # call transform on individual elements
+            # return array of transformed elements
+
+            [] # mocked return array for initial spec
+          end
+        end
+      end
+    end
+  end
+end
