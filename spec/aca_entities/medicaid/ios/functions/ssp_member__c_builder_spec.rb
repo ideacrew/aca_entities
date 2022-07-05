f15040cc065fae14e69a9132e74701a7ff96d645
mdkaraman <mdkaramanov@gmail.com>
2022-07-05 12:29:29 -0400

    set up ssp member builder and spec

--- spec/aca_entities/medicaid/ios/functions/ssp_member__c_builder_spec.rb
+++ spec/aca_entities/medicaid/ios/functions/ssp_member__c_builder_spec.rb
@@ -8,31 +8,33 @@ RSpec.describe AcaEntities::Medicaid::Ios::Functions::SspMemberCBuilder, dbclean
   # Consider using data prep plus transform pattern vs builder
   # (hard to spec the context object that builders use, plus they need data prep anyways)
 
-  # should use more recent example payload
+  # should use more recent example payload?
   let(:family) do
-    # family_json = File.read(Pathname.pwd.join("spec/support/atp/sample_payloads/simple_L_cv_payload.json"))
-    family_json = File.read(Pathname.pwd.join("/Users/michaelkaramanov/Desktop/testing/transfer_test_json.txt"))
+
+    family_json = File.read(Pathname.pwd.join("spec/support/atp/sample_payloads/uber_payload.json"))
     family_hash = JSON.parse(family_json)
     family_hash['family']
   end
   let(:applications) do
-    # need to use test payload that has array of applications (as opposed to single hash)
+    # need to use test payload that has array of applications (as opposed to single hash, or assume data prep prepares a hash)
     family['magi_medicaid_applications']
   end
 
   let(:context) do
-    context_hash = { magi_medicaid_applications: applications }
+    context_hash = { 'family.magi_medicaid_applications' => {
+      name: 'family.magi_medicaid_applications',
+      item: applications
+    } }
     AcaEntities::Operations::Transforms::Context.new(context_hash)
   end
 
   subject do
-    binding.irb
     described_class.new.call(context)
   end
 
   context 'with valid cv3 application passed in' do
-    it "should return success" do
-      expect(subject.success?).to be_truthy
+    it "should return an array" do
+      expect(subject).to be_a(Array)
     end
   end
 end
