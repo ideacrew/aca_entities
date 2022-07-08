0d5363e422cac9ca85a2b96fd92cad934c44608a
mdkaraman <mdkaramanov@gmail.com>
2022-07-08 10:44:06 -0400

    outline app indiv and benefits builder

--- lib/aca_entities/medicaid/ios/functions/ssp_application_individual__c_builder.rb
+++ lib/aca_entities/medicaid/ios/functions/ssp_application_individual__c_builder.rb
@@ -12,7 +12,18 @@ module AcaEntities
             # loop through applicants
             # build hash of field mappings
             # return array of transformed SSP_ApplicationIndividual__c hashes
-            [] # mocked return array for initial spec
+            # [] # mocked return array for initial spec
+
+            # NOTE: uncomment the family members code if data from family member is required
+            # family_members = @memoized_data.resolve('family.family_members_hash').item
+            application = @memoized_data.resolve('family.magi_medicaid_applications').item
+            applicants = application[:applicants]
+            applicants.map do |applicant|
+              # next unless applicant[:is_applying_coverage] # <- TODO: uncomment (unless we are sending non-applicants?)
+              # family_member = family_members.values.detect {|family_member| family_member[:hbx_id] == applicant[:person_hbx_id]}
+
+              {}
+            end
           end
         end
       end
