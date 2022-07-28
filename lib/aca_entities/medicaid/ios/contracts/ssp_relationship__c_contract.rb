44bb683de34c58fcaa41fa156a63dd7e1576d07d
Sarah Austin <scaustin34@gmail.com>
2022-07-28 10:45:21 -0400

    added back Id field to contract

--- lib/aca_entities/medicaid/ios/contracts/ssp_relationship__c_contract.rb
+++ lib/aca_entities/medicaid/ios/contracts/ssp_relationship__c_contract.rb
@@ -8,6 +8,7 @@ module AcaEntities
         class SSPRelationshipCContract < Dry::Validation::Contract
           params do
             required(:RelationshipType__c).filled(:string)
+            optional(:Id).maybe(:string)
             optional(:IsCareTakerToggle__c).maybe(:string)
             optional(:SSP_Member__c).maybe(:string)
             optional(:SSP_MemberRelatedTo__c).maybe(:string)
