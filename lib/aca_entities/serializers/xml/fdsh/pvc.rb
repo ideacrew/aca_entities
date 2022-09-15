fa395f16a46a33d3df43784ad7ae7fafe640e4aa
mdkaraman <mdkaramanov@gmail.com>
2022-09-15 13:31:13 -0400

    pvc serializers module

---
+++ lib/aca_entities/serializers/xml/fdsh/pvc.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "aca_entities/serializers/xml/fdsh/pvc/medicare/medicare"
+require "aca_entities/serializers/xml/fdsh/pvc/h43/pvc_manifest"
+
+module AcaEntities
+  module Serializers
+    module Xml
+      module Fdsh
+        # XML serialization for Pvc objects.
+        module Pvc
+        end
+      end
+    end
+  end
+end
