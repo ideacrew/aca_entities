3a42ae9a91185fb1009e66b09d9f8d9910f653e5
mdkaraman <mdkaramanov@gmail.com>
2022-10-10 17:39:06 -0400

    invert person association ref if flagged

--- lib/aca_entities/atp/functions/build_outbound_relationship.rb
+++ lib/aca_entities/atp/functions/build_outbound_relationship.rb
@@ -65,11 +65,11 @@ module AcaEntities
         }.freeze
 
         def call(cache)
-          @memoized_data = cache
-          applicants_hash = @memoized_data.resolve('family.magi_medicaid_applications.applicants').item
-          member_id = @memoized_data.find(/family.family_members.(\w+)$/).map(&:item).last
-          person_relationships = applicants_hash[member_id.to_sym][:mitc_relationships]
-          @primary_applicant_id = @memoized_data.find(Regexp.new('is_primary_applicant.*')).select {|a|  a.item == true}.first.name.split('.').last
+          @applicants_hash = cache.resolve('family.magi_medicaid_applications.applicants').item
+          family_flags = cache.resolve('family.family_flags').item
+          member_id = cache.find(/family.family_members.(\w+)$/).map(&:item).last
+          person_relationships = @applicants_hash[member_id.to_sym][:mitc_relationships]
+          person_relationships = find_relationships_to_member(member_id) if family_flags[:invert_person_association]
           return unless person_relationships
 
           person_relationships.uniq.each_with_object([]) do |person_relationship, collect|
@@ -80,6 +80,16 @@ module AcaEntities
             collect << { :person => { :ref => "pe#{person_relationship[:other_id]}" }, :family_relationship_code => atp_relationship_code,
                          :caretaker_dependent_code => nil }
           end
+        end
+
+        # collects relationships FROM other applicants TO applicant with member_id
+        def find_relationships_to_member(member_id)
+          @applicants_hash.each_with_object([]) do |applicant, collect|
+            applicant_id = applicant[0]
+            applicant_hash = applicant[1]
+            relationships = applicant_hash[:mitc_relationships].select {|rel| rel[:other_id] == member_id}
+            collect << relationships.each { |rel| rel[:other_id] = applicant_id }
+          end.flatten
         end
       end
     end
