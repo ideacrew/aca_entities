a2c84fd136b65139bb3b1bddb87ef6da3295ac9c
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2022-10-28 13:55:26 -0400

    Allow additional Protocol Options

--- lib/aca_entities/async_api/operations/find_configs_by_service_name.rb
+++ lib/aca_entities/async_api/operations/find_configs_by_service_name.rb
@@ -11,20 +11,27 @@ module AcaEntities
       class FindConfigsByServiceName
         send(:include, Dry::Monads[:result, :do, :try])
 
+        ProtocolOptionDefaults = { load_publisher_operations: true, load_subscriber_operations: true }.freeze
+
         # @param params [Hash] opts The options to initialize MagiMedicaidApplicationEntity
         # @option opts [String] :service_name AcaEntities::AsyncApi::Types::ServiceNameKind
         # @return [Dry::Monads::Result]
         def call(params)
           params ||= {}
+          options      = yield fetch_options(params[:options])
           service_name = yield validate_service_name(params)
           protocol = yield validate_protocol(params)
-          config_file_names = yield find_config_files(service_name)
+          config_file_names = yield find_config_files(service_name, options)
           config_params = yield parse_files(config_file_names, protocol)
 
           Success(config_params)
         end
 
         private
+
+        def fetch_options(options)
+          Success ProtocolOptionDefaults.merge(options || {})
+        end
 
         def validate_service_name(params)
           return Success(nil) unless params[:service_name]
@@ -38,13 +45,18 @@ module AcaEntities
           value.success? ? Success(value.input) : Failure(value.error)
         end
 
-        def find_config_files(service_name)
+        def find_config_files(service_name, options)
           files =
-            if service_name
+            if service_name && options[:load_publisher_operations] && options[:load_subscriber_operations]
               Gem.find_files("aca_entities/async_api/#{service_name}/**/*.yml")
+            elsif service_name && options[:load_publisher_operations]
+              Gem.find_files("aca_entities/async_api/#{service_name}/**/*_publish.yml")
+            elsif service_name && options[:load_subscriber_operations]
+              Gem.find_files("aca_entities/async_api/#{service_name}/**/*_subscribe.yml")
             else
               Gem.find_files("aca_entities/async_api/**/*.yml")
             end
+
           Success(files)
         end
 
