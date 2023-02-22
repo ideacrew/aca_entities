a585075cf5639fa0c92584a1ae50dacbffa172e3
Raghu Ram <raghuramg83@gmail.com>
2023-02-22 18:15:58 -0500

    add corrected/void attributes to aptc_csr_tax_households (#321)
    * add corrected/void fields
    
    * fixed types in entity

--- lib/aca_entities/insurance_policies/aca_individuals/aptc_csr_tax_household.rb
+++ lib/aca_entities/insurance_policies/aca_individuals/aptc_csr_tax_household.rb
@@ -5,6 +5,8 @@ module AcaEntities
     module AcaIndividuals
       class AptcCsrTaxHousehold < Dry::Struct
         attribute :hbx_assigned_id, Types::String.optional.meta(omittable: true)
+        attribute :corrected, Types::Bool.optional.meta(omittable: true)
+        attribute :void, Types::Bool.optional.meta(omittable: true)
         attribute :tax_household_members,
                   Types::Strict::Array.of(AcaEntities::Households::TaxHouseholdMember).meta(omittable: false)
         attribute :covered_individuals,
