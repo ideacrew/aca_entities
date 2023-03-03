ef284e19a2f68ca80c7ccccec71afdefa0933cb2
mdkaraman <mdkaramanov@gmail.com>
2023-03-03 16:39:29 -0500

    pay now domain objects library

---
+++ lib/aca_entities/pay_now/care_first.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# types
+require 'aca_entities/types'
+
+# contracts
+require_relative "care_first/contracts/coverage_kind_contract"
+
+# entities
+require_relative "care_first/coverage_kind"
+
+# transformers
+require_relative 'care_first/transformers/'
+
+# operations
+require_relative 'care_first/operations/generate_xml'
+
+# happymapper
+require 'aca_entities/serializers/xml/pay_now/care_first/'
+
+module AcaEntities
+  module PayNow
+    # Domain objects representing CareFirst custom XML
+    module CareFirst
+    end
+  end
+end
