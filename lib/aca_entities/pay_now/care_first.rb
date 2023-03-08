454315bb1b8430594cee51b59609b8f82dcafea7
Michael Karamanov <mdkaramanov@gmail.com>
2023-03-08 11:54:00 -0500

    add initial entities, contracts and serializers for CF Pay Now payload (#328)
    * add initial entities, contracts and serializers
    
    * tskip requiring operation and transform

---
+++ lib/aca_entities/pay_now/care_first.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# types
+require 'aca_entities/types'
+require_relative 'care_first/types'
+
+# contracts
+require_relative 'care_first/contracts/pay_now_transfer_payload_contract'
+
+# entities
+require_relative 'care_first/pay_now_transfer_payload'
+
+# transformers
+# require_relative 'care_first/transformers/coverage_and_members'
+
+# operations
+# require_relative 'care_first/operations/generate_xml'
+
+# happymapper
+require 'aca_entities/serializers/xml/pay_now/care_first/coverage_kind'
+require 'aca_entities/serializers/xml/pay_now/care_first/pay_now_transfer_payload'
+
+module AcaEntities
+  module PayNow
+    # Domain objects representing CareFirst custom XML
+    module CareFirst
+    end
+  end
+end
