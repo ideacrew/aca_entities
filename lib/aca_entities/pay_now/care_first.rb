055e83ec9bb8df8b7f36c3f220547ae4ff82337d
mdkaraman <mdkaramanov@gmail.com>
2023-03-08 14:49:49 +0000

    tskip requiring operation and transform

--- lib/aca_entities/pay_now/care_first.rb
+++ lib/aca_entities/pay_now/care_first.rb
@@ -11,10 +11,10 @@ require_relative 'care_first/contracts/pay_now_transfer_payload_contract'
 require_relative 'care_first/pay_now_transfer_payload'
 
 # transformers
-require_relative 'care_first/transformers/coverage_and_members'
+# require_relative 'care_first/transformers/coverage_and_members'
 
 # operations
-require_relative 'care_first/operations/generate_xml'
+# require_relative 'care_first/operations/generate_xml'
 
 # happymapper
 require 'aca_entities/serializers/xml/pay_now/care_first/coverage_kind'
