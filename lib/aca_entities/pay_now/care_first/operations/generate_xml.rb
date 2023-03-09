7528dde9bced3905a42fff03db8d52adc25c5686
mdkaraman <mdkaramanov@gmail.com>
2023-03-09 14:56:12 -0500

    initial mapping experimentation

--- lib/aca_entities/pay_now/care_first/operations/generate_xml.rb
+++ lib/aca_entities/pay_now/care_first/operations/generate_xml.rb
@@ -13,9 +13,12 @@ module AcaEntities
           include Dry::Monads[:result, :do, :try]
 
           def call(payload)
+            # TODO: validate enrollment and members with aca_entities contracts
+            # validated_payload = yield validate_payload(payload)
             transformed_payload = yield transform_payload(payload)
-            validated_payload = yield validate_payload(transformed_payload)
-            entity = yield initialize_entity(validated_payload)
+            binding.irb
+            validated_transformed_payload = yield validate_transformed_payload(transformed_payload)
+            entity = yield initialize_entity(validated_transformed_payload)
             serialized_payload = yield to_serialized_obj(entity)
             xml_payload = serialized_payload.to_xml
             Success(xml_payload)
@@ -30,7 +33,7 @@ module AcaEntities
             Failure("CoverageAndMembers transformer #{e}")
           end
 
-          def validate_payload(params)
+          def validate_transformed_payload(params)
             result = Try do
               AcaEntities::PayNow::CareFirst::Contracts::PayNowTransferPayloadContract.new.call(params)
             end.to_result
--- lib/aca_entities/pay_now/care_first/transformers/coverage_and_members.rb
+++ lib/aca_entities/pay_now/care_first/transformers/coverage_and_members.rb
@@ -16,7 +16,36 @@ module AcaEntities
               }
 
               # TODO: primary
+
               # TODO: members
+              namespace 'members' do
+                rewrap 'members', type: :array do
+                  rewrap '', type: :hash do
+                    # namespace 'member' do
+                      # rewrap 'member', type: :hash do
+
+
+                        # map 'hbx_id', 'hbx_id', memoize: true, visible: false
+                        # add_key 'exchange_assigned_member_id', function: lambda {|v|
+                        #   binding.irb
+                        #   v.find('hbx_id').first.item }
+                        map 'hbx_id', 'member.exchange_assigned_member_id'
+                        map 'person_name.first_name', 'member.member_name.person_given_name'
+                        map 'person_name.middle_name', 'member.member_name.person_middle_name'
+                        map 'person_name.last_name', 'member.member_name.person_surname'
+                        map 'person_name.full_name', 'member.member_name.person_full_name'
+                        map 'person_name.name_pfx', 'member.member_name.person_name_prefix_text'
+                        map 'person_name.name_sfx', 'member.member_name.person_name_suffix_text'
+                        map 'person_name.alternate_name', 'member.member_name.person_alternate_name'
+                        
+                        map 'person_demographics.dob', 'member.birth_date', function: lambda {|birth_date|
+                          binding.irb
+                        }
+                      # end
+                    # end
+                  end
+                end
+              end
             end
           end
         end
