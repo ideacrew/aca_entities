09c8a748a8065b5a22fcfd476e1be182abd55531
Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
2023-03-10 12:58:02 -0500

    completing primary tag by adding to pay now transfer payload parent tag (#334)
    * first pass at adding member tag
    
    * fixed spec, change birthdate to string
    
    * removed unnecessary namespacing
    
    * removed debugging puts
    
    * fixed require ordering
    
    * added members array to PayNowTransferPayload
    
    * rubocop
    
    * marked spec as pending until transform piece complete
    
    * adding primary tag to pay_now_transfer_payload
    
    * fixing domain mapper issue
    
    * rubocop fix
    
    ---------
    
    Signed-off-by: Sarah Austin <scaustin34@gmail.com>
    Signed-off-by: Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
    Co-authored-by: Sarah Austin <scaustin34@gmail.com>
    Co-authored-by: Michael Karamanov <mdkaramanov@gmail.com>

--- spec/aca_entities/pay_now/care_first/pay_now_transfer_payload_spec.rb
+++ spec/aca_entities/pay_now/care_first/pay_now_transfer_payload_spec.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-require 'aca_entities/pay_now/care_first'
+require "spec_helper"
+require "aca_entities/pay_now/care_first"
 
 RSpec.describe ::AcaEntities::PayNow::CareFirst::PayNowTransferPayload, dbclean: :after_each do
-
   let(:member_name) do
     { :person_surname => "Thomas",
       :person_given_name => "Jeff" }
   end
+
+  let(:primary) do
+    { :exchange_assigned_member_id => "1037294",
+      :member_name => member_name }
+  end
+
   let(:member) do
     { :exchange_assigned_member_id => "1037294",
       :member_name => member_name,
@@ -20,29 +25,30 @@ RSpec.describe ::AcaEntities::PayNow::CareFirst::PayNowTransferPayload, dbclean:
 
   let(:required_params) do
     { coverage_kind: AcaEntities::PayNow::CareFirst::Types::PaynowCoverageKindType.values.first,
+      primary: primary,
       members: [member] }
   end
   let(:optional_params) { {} }
   let(:all_params) { required_params.merge(optional_params) }
   let(:invalid_params) do
-    { bad_key: 'bad_val' }
+    { bad_key: "bad_val" }
   end
 
-  context 'invalid parameters' do
-    context 'with empty parameters' do
-      it 'should list error for every required parameter' do
-        expect { described_class.new }.to raise_error(Dry::Struct::Error, /:coverage_kind is missing in Hash input/)
+  context "invalid parameters" do
+    context "with empty parameters" do
+      it "should list error for every required parameter" do
+        expect { described_class.new }.to raise_error(Dry::Struct::Error, /:person_surname is missing in Hash input/)
       end
     end
   end
 
-  context 'valid parameters' do
-    context 'with required parameters only' do
-      it 'should initialize' do
+  context "valid parameters" do
+    context "with required parameters only" do
+      it "should initialize" do
         expect(described_class.new(required_params)).to be_a described_class
       end
 
-      it 'should not raise error' do
+      it "should not raise error" do
         expect { described_class.new(required_params) }.not_to raise_error
       end
     end
--- spec/aca_entities/serializers/xml/pay_now/care_first/pay_now_transfer_payload_spec.rb
+++ spec/aca_entities/serializers/xml/pay_now/care_first/pay_now_transfer_payload_spec.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-require 'aca_entities/pay_now/care_first'
-require 'aca_entities/serializers/xml/pay_now/care_first'
+require "spec_helper"
+require "aca_entities/pay_now/care_first"
+require "aca_entities/serializers/xml/pay_now/care_first"
 
 RSpec.describe AcaEntities::Serializers::Xml::PayNow::CareFirst::PayNowTransferPayload do
   let(:member_name) do
     { :person_surname => "Thomas",
       :person_given_name => "Jeff" }
   end
+
+  let(:primary) do
+    { :exchange_assigned_member_id => "1037294",
+      :member_name => member_name }
+  end
+
   let(:member) do
     { :exchange_assigned_member_id => "1037294",
       :member_name => member_name,
@@ -20,8 +26,10 @@ RSpec.describe AcaEntities::Serializers::Xml::PayNow::CareFirst::PayNowTransferP
   end
   let(:payload_hash) do
     { :coverage_kind => "urn:openhbx:terms:v1:qhp_benefit_coverage#health",
+      :primary => primary,
       :members => [member] }
   end
+
   let(:entity) do
     AcaEntities::PayNow::CareFirst::PayNowTransferPayload.new(payload_hash)
   end
@@ -30,11 +38,11 @@ RSpec.describe AcaEntities::Serializers::Xml::PayNow::CareFirst::PayNowTransferP
     AcaEntities::Serializers::Xml::PayNow::CareFirst::PayNowTransferPayload.domain_to_mapper(entity)
   end
 
-  it 'should generate and xml string' do
+  it "should generate and xml string" do
     expect(subject.to_xml.split("\n").first).to eq '<?xml version="1.0"?>'
   end
 
-  it 'creates a schema valid result' do
+  it "creates a schema valid result" do
     # add validation operation and get result here
     # expect(validation_result.success?).to be_truthy
   end
