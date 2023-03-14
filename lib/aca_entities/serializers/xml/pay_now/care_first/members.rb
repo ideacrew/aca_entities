4933b92ea339e4276ef94b029316e53bd9f84bc3
mdkaraman <mdkaramanov@gmail.com>
2023-03-14 18:25:40 +0000

    rubocop

--- lib/aca_entities/serializers/xml/pay_now/care_first/members.rb
+++ lib/aca_entities/serializers/xml/pay_now/care_first/members.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
 module AcaEntities
-    module Serializers
-      module Xml
-        module PayNow
-          module CareFirst
-            # Happymapper implementation for Member.
-            class Members
-              include HappyMapper
-  
-              tag 'members'
+  module Serializers
+    module Xml
+      module PayNow
+        module CareFirst
+          # Happymapper implementation for Member.
+          class Members
+            include HappyMapper
 
-              has_many :members, Member
-  
-              def self.domain_to_mapper(members)
-                mapper = self.new
-                mapper.members = members.map do |member|
-                    Member.domain_to_mapper(member)
-                end
-                mapper
+            tag 'members'
+
+            has_many :members, Member
+
+            def self.domain_to_mapper(members)
+              mapper = self.new
+              mapper.members = members.map do |member|
+                Member.domain_to_mapper(member)
               end
+              mapper
             end
           end
         end
       end
     end
   end
+end
