70846aa2507e945e0710fac5678ca2377d743367
Sarah Austin <scaustin34@gmail.com>
2023-05-02 13:05:29 -0400

    add income type, add date range to non-esi node map, added spec and updated ex payload

--- lib/aca_entities/atp/functions/applicant_builder.rb
+++ lib/aca_entities/atp/functions/applicant_builder.rb
@@ -7,7 +7,7 @@ module AcaEntities
     module Functions
       # applicants builder
       class ApplicantBuilder
-        def call(cache) # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+        def call(cache) # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity
           applicants_hash = cache.resolve('family.magi_medicaid_applications.applicants').item
           applicants_hash.each_with_object([]) do |applicant_hash, collector|
             applicant = applicant_hash[1]
@@ -21,24 +21,35 @@ module AcaEntities
             esi_coverage_enrolled = enrolled_benefits.select {|benefit|  benefit[:kind] == "employer_sponsored_insurance" }
             esi_coverage_eligible = eligible_benefits.select {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
 
-            non_esi_coverage_indicators = if non_esi_coverage_enrolled_array.present?
-                                            { non_esi_coverage_indicators: [true] }
-                                          else
-                                            { non_esi_coverage_indicators: [false] }
-                                          end
+            non_esi_coverage_indicators =
+              if non_esi_coverage_enrolled_array.present?
+                { non_esi_coverage_indicators: [true] }
+              else
+                { non_esi_coverage_indicators: [false] }
+              end
 
-            non_esi_policies = if non_esi_coverage_eligible_array
-                                 non_esi_coverage_eligible_array.collect do |benefit|
-                                   source_code = AcaEntities::Atp::Types::InsuranceKindsOutbound[benefit[:kind]]
-                                   { source_code: source_code } if source_code
-                                 end.compact
-                               else
-                                 []
-                               end
+            non_esi_policies =
+              if non_esi_coverage_eligible_array
+                non_esi_coverage_eligible_array.collect do |benefit|
+                  start_on = Date.parse(benefit[:start_on]) rescue nil
+                  end_on = Date.parse(benefit[:end_on]) rescue nil
+                  {
+                    source_code: AcaEntities::Atp::Types::InsuranceKindsOutbound[benefit[:kind]],
+                    applied_effective_date_range: {
+                      start_date: { date: start_on },
+                      end_date: { date: end_on }
+                    }
+                  }
+                end.compact
+              else
+                []
+              end
 
-            esi_coverage_indicators = [{ enrolled_indicator: esi_coverage_enrolled.present?,
-                                         eligible_indicator: esi_coverage_eligible.present?,
-                                         eligibility_unknown_indicator: !(esi_coverage_enrolled.present? || esi_coverage_eligible.present?) }]
+            esi_coverage_indicators = [{
+              enrolled_indicator: esi_coverage_enrolled.present?,
+              eligible_indicator: esi_coverage_eligible.present?,
+              eligibility_unknown_indicator: !(esi_coverage_enrolled.present? || esi_coverage_eligible.present?)
+            }]
 
             insurance_applicant = AcaEntities::Atp::Transformers::Aces::Applicant.transform(applicant)
             insurance_applicant.merge!(non_esi_coverage_indicators, esi_associations: esi_coverage_indicators, non_esi_policies: non_esi_policies)
