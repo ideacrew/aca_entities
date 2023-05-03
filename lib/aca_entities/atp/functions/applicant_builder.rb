674aa71c5cfaea8f44193f97a5b833163dc582f4
Sarah Austin <scaustin34@gmail.com>
2023-05-03 14:22:17 -0400

    refactor to remove rubocop disables

--- lib/aca_entities/atp/functions/applicant_builder.rb
+++ lib/aca_entities/atp/functions/applicant_builder.rb
@@ -7,7 +7,7 @@ module AcaEntities
     module Functions
       # applicants builder
       class ApplicantBuilder
-        def call(cache) # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity
+        def call(cache)
           applicants_hash = cache.resolve('family.magi_medicaid_applications.applicants').item
           applicants_hash.each_with_object([]) do |applicant_hash, collector|
             applicant = applicant_hash[1]
@@ -21,27 +21,8 @@ module AcaEntities
             esi_coverage_enrolled = enrolled_benefits.select {|benefit|  benefit[:kind] == "employer_sponsored_insurance" }
             esi_coverage_eligible = eligible_benefits.select {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
 
-            non_esi_coverage_indicators =
-              if non_esi_coverage_enrolled_array.present?
-                { non_esi_coverage_indicators: [true] }
-              else
-                { non_esi_coverage_indicators: [false] }
-              end
-
-            non_esi_policies =
-              if non_esi_coverage_eligible_array
-                non_esi_coverage_eligible_array.collect do |benefit|
-                  kind = benefit[:kind].present? ? AcaEntities::Atp::Types::InsuranceKindsOutbound[benefit[:kind]] : nil
-                  start_on = benefit[:start_on].present? ? Date.parse(benefit[:start_on]) : nil
-                  end_on = benefit[:end_on].present? ? Date.parse(benefit[:end_on]) : nil
-                  {
-                    source_code: kind,
-                    applied_effective_date_range: { start_date: { date: start_on }, end_date: { date: end_on } }
-                  }
-                end.compact
-              else
-                []
-              end
+            non_esi_coverage_indicators = non_esi_coverage_indicators(non_esi_coverage_enrolled_array)
+            non_esi_policies = get_non_esi_policies(non_esi_coverage_eligible_array)
 
             esi_coverage_indicators = [{
               enrolled_indicator: esi_coverage_enrolled.present?,
@@ -54,6 +35,31 @@ module AcaEntities
             collector << insurance_applicant
           end
         end
+
+        def non_esi_coverage_indicators(non_esi_coverage_enrolled_array)
+          if non_esi_coverage_enrolled_array.present?
+            { non_esi_coverage_indicators: [true] }
+          else
+            { non_esi_coverage_indicators: [false] }
+          end
+        end
+
+        def get_non_esi_policies(non_esi_coverage_eligible_array)
+          if non_esi_coverage_eligible_array
+            non_esi_coverage_eligible_array.collect do |benefit|
+              kind = benefit[:kind].present? ? AcaEntities::Atp::Types::InsuranceKindsOutbound[benefit[:kind]] : nil
+              start_on = benefit[:start_on].present? ? Date.parse(benefit[:start_on]) : nil
+              end_on = benefit[:end_on].present? ? Date.parse(benefit[:end_on]) : nil
+              {
+                source_code: kind,
+                applied_effective_date_range: { start_date: { date: start_on }, end_date: { date: end_on } }
+              }
+            end.compact
+          else
+            []
+          end
+        end
+
       end
     end
   end
