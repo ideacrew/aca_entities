012ebc1239267a1706966fd9bec6600e76579c62
Sarah Austin <scaustin34@gmail.com>
2023-05-10 15:28:25 -0400

    refactor for readability

--- lib/aca_entities/atp/functions/applicant_builder.rb
+++ lib/aca_entities/atp/functions/applicant_builder.rb
@@ -14,39 +14,40 @@ module AcaEntities
             next unless applicant[:is_applying_coverage]
 
             enrolled_benefits = applicant[:benefits].select {|benefit| benefit[:status] == "is_enrolled" }
+            enrolled_esi_benefits = enrolled_benefits.select {|benefit|  benefit[:kind] == "employer_sponsored_insurance" }
+            enrolled_non_esi_benfits = enrolled_benefits.reject {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
+
             eligible_benefits = applicant[:benefits].select {|benefit| benefit[:status] == "is_eligible" }
+            eligible_esi_benefits = eligible_benefits.select {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
+            eligible_non_esi_benefits = eligible_benefits.reject {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
 
-            non_esi_coverage_enrolled_array = enrolled_benefits.reject {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
-            non_esi_coverage_eligible_array = eligible_benefits.reject {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
-            esi_coverage_enrolled = enrolled_benefits.select {|benefit|  benefit[:kind] == "employer_sponsored_insurance" }
-            esi_coverage_eligible = eligible_benefits.select {|benefit| benefit[:kind] == "employer_sponsored_insurance" }
-
-            non_esi_coverage_indicators = non_esi_coverage_indicators(non_esi_coverage_enrolled_array)
-            non_esi_policies = get_non_esi_policies(non_esi_coverage_eligible_array)
+            enrolled_non_esi_indicator = enrolled_non_esi_indicator(enrolled_non_esi_benfits)
 
             esi_coverage_indicators = [{
-              enrolled_indicator: esi_coverage_enrolled.present?,
-              eligible_indicator: esi_coverage_eligible.present?,
-              eligibility_unknown_indicator: !(esi_coverage_enrolled.present? || esi_coverage_eligible.present?)
+              enrolled_indicator: enrolled_esi_benefits.present?,
+              eligible_indicator: eligible_esi_benefits.present?,
+              eligibility_unknown_indicator: !(enrolled_esi_benefits.present? || eligible_esi_benefits.present?)
             }]
 
+            non_esi_policies = get_non_esi_policies(eligible_non_esi_benefits)
+
             insurance_applicant = AcaEntities::Atp::Transformers::Aces::Applicant.transform(applicant)
-            insurance_applicant.merge!(non_esi_coverage_indicators, esi_associations: esi_coverage_indicators, non_esi_policies: non_esi_policies)
+            insurance_applicant.merge!(enrolled_non_esi_indicator, esi_associations: esi_coverage_indicators, non_esi_policies: non_esi_policies)
             collector << insurance_applicant
           end
         end
 
-        def non_esi_coverage_indicators(non_esi_coverage_enrolled_array)
-          if non_esi_coverage_enrolled_array.present?
+        def enrolled_non_esi_indicator(enrolled_non_esi_benfits)
+          if enrolled_non_esi_benfits.present?
             { non_esi_coverage_indicators: [true] }
           else
             { non_esi_coverage_indicators: [false] }
           end
         end
 
-        def get_non_esi_policies(non_esi_coverage_eligible_array)
-          if non_esi_coverage_eligible_array
-            non_esi_coverage_eligible_array.collect do |benefit|
+        def get_non_esi_policies(eligible_non_esi_benefits)
+          if eligible_non_esi_benefits
+            eligible_non_esi_benefits.collect do |benefit|
               kind = benefit[:kind].present? ? AcaEntities::Atp::Types::InsuranceKindsOutbound[benefit[:kind]] : nil
               start_on = benefit[:start_on].present? ? Date.parse(benefit[:start_on]) : nil
               end_on = benefit[:end_on].present? ? Date.parse(benefit[:end_on]) : nil
