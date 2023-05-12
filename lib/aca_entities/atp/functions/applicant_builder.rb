eefb33c843e6602e6787bea0b66efeb25fdeac0f
Sarah Austin <scaustin34@gmail.com>
2023-05-12 10:58:37 -0400

    send details for enrolled non-esi instead of eligible

--- lib/aca_entities/atp/functions/applicant_builder.rb
+++ lib/aca_entities/atp/functions/applicant_builder.rb
@@ -21,7 +21,7 @@ module AcaEntities
 
             enrolled_non_esi_indicator = get_enrolled_non_esi_indicator(enrolled_non_esi_benfits)
             esi_coverage_indicators = get_esi_coverage_indicators(esi_benefits)
-            non_esi_policies = get_non_esi_policies(eligible_non_esi_benefits)
+            non_esi_policies = get_non_esi_policies(enrolled_non_esi_benfits)
 
             insurance_applicant = AcaEntities::Atp::Transformers::Aces::Applicant.transform(applicant)
             insurance_applicant.merge!(enrolled_non_esi_indicator, esi_associations: esi_coverage_indicators, non_esi_policies: non_esi_policies)
@@ -56,9 +56,9 @@ module AcaEntities
           end
         end
 
-        def get_non_esi_policies(eligible_non_esi_benefits)
-          if eligible_non_esi_benefits
-            eligible_non_esi_benefits.collect do |benefit|
+        def get_non_esi_policies(non_esi_benefits)
+          if non_esi_benefits
+            non_esi_benefits.collect do |benefit|
               kind = benefit[:kind].present? ? AcaEntities::Atp::Types::InsuranceKindsOutbound[benefit[:kind]] : nil
               start_on = benefit[:start_on].present? ? Date.parse(benefit[:start_on]) : nil
               end_on = benefit[:end_on].present? ? Date.parse(benefit[:end_on]) : nil
