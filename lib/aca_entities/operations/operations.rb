2dcee6b3b1115f88a9782103ca91895e9bb2f849
Raghu Ram <raghuramg83@gmail.com>
2023-06-20 13:57:00 -0400

    add audit log publish/subscribe ymls (#359)
    * add audit log publish/subscribe ymls
    
    * updates for audit log exchange name
    
    * update for description
    
    * add composite operation
    
    * removed duplicate require statements
    
    * update for core library operations
    
    * added specs for composite operation
    
    * rubocop fixes

---
+++ lib/aca_entities/operations/operations.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'operation'
+require_relative 'composite_operation'
+require_relative 'files/read'
+require_relative 'yaml/deserialize'
+require_relative 'yaml/serialize'
+require_relative 'encryption/encrypt'
+require_relative 'encryption/decrypt'
+require_relative 'mongoid/model_adapter'
+require_relative 'mongoid/entity_adapter'
+
+module AcaEntities
+  # Operations are perform Functions on domain entities
+  module Operations
+  end
+end
