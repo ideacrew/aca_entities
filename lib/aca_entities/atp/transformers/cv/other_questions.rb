8b7fb88ff5717aaf4ccdb03e456e20f9ee49f50d
Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
2023-06-28 13:27:38 -0400

    creating mapping to post partum/pregnancy based on end date if sent (#376)
    * differentiating between pregnancy due date and end date in atp mapping
    
    * creating mapping to post partum/pregnancy based on end date if sent
    
    * clearing up logic
    
    * updating spec
    
    Signed-off-by: Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Esme Rubinstein <51678367+erubinst@users.noreply.github.com>

--- lib/aca_entities/atp/transformers/cv/other_questions.rb
+++ lib/aca_entities/atp/transformers/cv/other_questions.rb
@@ -20,11 +20,9 @@ module AcaEntities
           add_key 'student.student_school_kind', value: nil # default
           add_key 'student.student_status_end_on', value: nil # default
 
-          map 'pregnancy.status_indicator', 'pregnancy_information.is_pregnant', function: ->(v) { v || false}
-          map 'pregnancy.expected_baby_quantity', 'pregnancy_information.expected_children_count'
-          add_key 'pregnancy_information.is_enrolled_on_medicaid', value: nil # default
-          add_key 'pregnancy_information.is_post_partum_period', value: nil # default
+          # create memoized context for pregnancy end date
           map "pregnancy.status_valid_date_range.end_date.date", "pregnancy_end_date", memoize: true, visible: false
+          # populate due date if future date, end date if past/present date
           add_key "pregnancy_information.pregnancy_due_on", function: lambda { |v|
             return nil unless v
             date = v.resolve('pregnancy_end_date').item
@@ -39,6 +37,28 @@ module AcaEntities
             end_date = Date.parse(date)
             end_date <= Date.today ? end_date : nil
           }
+          # create memoized context for pregnancy status indicator
+          map 'pregnancy.status_indicator', 'pregnancy_status_indicator', memoize: true, visible: false
+          # dc uses pregnancy status to indicate both pregnancy and post-partum, use pregnancy end date to determine if post-partum or pregnant
+          add_key 'pregnancy_information.is_pregnant', function: lambda { |v|
+            return false unless v
+            end_date = v.resolve('pregnancy_end_date').item
+            pregnancy_status = v.resolve('pregnancy_status_indicator').item
+            return false unless pregnancy_status
+            # preserving ME behavior by returning true if pregnancy status is true and end date is nil
+            return true unless end_date
+            Date.parse(end_date) > Date.today
+          }
+          # post partum period defaults to nil to preserve ME behavior, default will be set to false for DC in MG before sending to Enroll
+          add_key 'pregnancy_information.is_post_partum_period', function: lambda { |v|
+            return nil unless v
+            end_date = v.resolve('pregnancy_end_date').item
+            pregnancy_status = v.resolve('pregnancy_status_indicator').item
+            return nil unless end_date && pregnancy_status
+            Date.parse(end_date) <= Date.today ? true : nil
+          }
+          map 'pregnancy.expected_baby_quantity', 'pregnancy_information.expected_children_count'
+          add_key 'pregnancy_information.is_enrolled_on_medicaid', value: nil # default
 
           map 'incarcerations.incarceration_indicator', 'attestation.is_incarcerated'
           map 'blindness_or_disability_indicator', 'attestation.is_self_attested_disabled', memoize: true, visible: true, function: lambda { |v|
