15b99425152d3f3e56d54783d540af0e742fe8a3
haridhar yamjala <hariym471@gmail.com>
2023-07-07 13:01:46 -0400

    Add transmittable domain model 185007548 (#368)
    * initial commit
    
    * added initial test of composite operation
    
    * Add documentation
    Fix rubocop errors
    
    * added more test regarding sub operations
    
    * Add #child_operation_names
    Add guards to #add_child_operation and #dro_child_operation
    Add specs and documentation
    
    * Update initialization to match CompositeOperation
    
    * Add Job & Transmission Entities
    
    * Change namespace to Protocols/Transmitable
    Added spec for contracts/job_contract_spec
    
    * Move started_at/ended_at rule to contract
    Added transaction_contract and spec
    
    * Add rules for nested transmission and transaction
    validations
    
    * Add Transmission entity
    Fix namespace errors
    Add yard documentation
    
    * Fix namespace typo (Transmitable)
    Add Subject entity and spec
    
    * Work in progress handoff to dev team
    
    * started resolving spec failures
    
    * spec fixes and green clean up
    
    * transaction transmissions
    
    * spec fixes
    
    * removes transmission id on transmission
    
    * changes name to key
    
    * adding transactions_transmissions association
    
    * resolved spec issues related to transactions_transmissions
    
    * resolved issues found while implementing in FDSH
    
    * Minor updates to work with fdsh
    
    * update transmittable model with new structure per discussion
    
    * Transmittable model final update
    
    * change payload name and type
    
    * spec coverage for process status and minor updates
    
    ---------
    
    Signed-off-by: kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
    Co-authored-by: Marco Ornelas <marco.ornelas@ideacrew.com>
    Co-authored-by: ipublic <dan@ideacrew.com>
    Co-authored-by: Polo Ornelas <me@poloornelas.mx>
    Co-authored-by: Kristin Merbach <kristin@kristinmerbach.com>
    Co-authored-by: kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>

---
+++ lib/aca_entities/protocols/transmittable/contracts/contract.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'dry/validation'
+
+Dry::Validation.load_extensions(:monads)
+
+# Configuration values and shared rules and macros for domain model validation contracts
+module AcaEntities
+  module Protocols
+    module Transmittable
+      module Contracts
+        # Transmittable module Contract class (inherits from the {AcaEntities::Contracts::Contract}) top level contract class
+        class Contract < AcaEntities::Contracts::Contract
+          # config.messages.namespace - custom messages namespace for a contract class. Use this to differentiate common messages
+
+          rule(:ended_at, :started_at) do
+            if keys && values[:started_at].present? && values[:ended_at].present? && values[:ended_at] < values[:started_at]
+              key.failure('must be after started_at')
+            end
+          end
+
+          # rule(:transactions).each do |index:|
+          #   next unless key? && value.is_a?(Hash)
+          #   values.to_h[:transactions[index]]&.each_pair do |attr_key, attr_val|
+          #     result = TransactionContract.new.call(attr_val)
+          #     next unless result.failure?
+
+          #     key([*path, attr_key]).failure(
+          #       { text: 'error', code: result.errors.to_h }
+          #     )
+          #   end
+          # end
+
+          # rule(:transmissions).each do |index:|
+          #   next unless key? && value.is_a?(Hash)
+          #   values.to_h[:transmissions[index]]&.each_pair do |attr_key, attr_val|
+          #     result = TransmissionContract.new.call(attr_val)
+          #     next unless result.failure?
+
+          #     key([*path, attr_key]).failure(
+          #       { text: 'error', code: result.errors.to_h }
+          #     )
+          #   end
+          # end
+
+        end
+      end
+    end
+  end
+end
---
+++ lib/aca_entities/protocols/transmittable/types.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'dry-types'
+
+module AcaEntities
+  module Protocols
+    module Transmittable
+      # Constants, enumerations and type definitions for {Transmittable} entities
+      module Types
+        send(:include, Dry.Types())
+        # send(:include, Dry.Types)
+        send(:include, Dry::Logic)
+
+        ProcessStateKeyKinds = Types::Coercible::Symbol.enum(
+          :initial,
+          :transmitted,
+          :received,
+          :acked,
+          :nacked,
+          :retried,
+          :succeeded,
+          :failed,
+          :expired
+        ).freeze
+
+        DefaultTransmitActionKind =
+          Types::Coercible::String.enum(:blocked, :expired, :hold, :no_transmit, :pending, :transmit).freeze
+
+        DefaultTransmissionStatusKind = Types::Coercible::String.enum('open', 'pending', 'processing', 'transmitted').freeze
+
+        # A list of valid status values.  Override defaults using initializer options
+        # acked: acknowledged
+        # completed: processing of the object finished
+        # nacked: negative_acknowledged, an outside service completed processing and indicated an error
+        # pending: awaiting processing
+        # blocked: transaction status when we do not need to transmit the latest transaction,
+        # usually upon receiving void when we never transmitted before
+        DefaultTransactionStatusKind =
+          Types::Coercible::String.enum(
+            'approved',
+            'acked',
+            'blocked',
+            'created',
+            'completed',
+            'denied',
+            'errored',
+            'excluded',
+            'expired',
+            'failed',
+            'nacked',
+            'pending',
+            'rejected',
+            'submitted',
+            'successful',
+            'superseded',
+            'transmitted'
+          ).freeze
+      end
+    end
+  end
+end
