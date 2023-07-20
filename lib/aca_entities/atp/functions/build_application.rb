5d6e6cd18a502a3b2530366af6461e77a138de98
Esme Rubinstein <erubinst@oberlin.edu>
2023-07-20 16:00:46 -0400

    fixing transformer call for deductions in transformer

--- lib/aca_entities/atp/functions/build_application.rb
+++ lib/aca_entities/atp/functions/build_application.rb
@@ -129,7 +129,8 @@ module AcaEntities
           @expenses_hash.each_with_object([]) do |expense, result|
             next if AcaEntities::Atp::Types::DeductionKinds[expense[:category_code]].nil?
 
-            result << AcaEntities::Atp::Transformers::Cv::Income.transform(expense)
+            result << AcaEntities::Atp::Transformers::Cv::Deduction.transform(expense)
+            binding.irb
             result
           end
         end
