1b754d12a9af16c1ed9c75c76d1f09f9748c1818
Vishal Kalletla <vishuk1201@gmail.com>
2023-10-25 13:22:59 -0400

    add specs

--- spec/aca_entities/serializers/xml/fdsh/vlp/h92/operations/initial_request_to_xml_spec.rb
+++ spec/aca_entities/serializers/xml/fdsh/vlp/h92/operations/initial_request_to_xml_spec.rb
@@ -4,44 +4,106 @@ require 'spec_helper'
 
 RSpec.describe ::AcaEntities::Serializers::Xml::Fdsh::Vlp::H92::Operations::InitialRequestToXml, dbclean: :after_each do
 
-  let(:initial_verification_request_entity) do
-    AcaEntities::Fdsh::Vlp::H92::InitialVerificationRequest.call(initial_verification_request)
+  context 'with valid arguments' do
+    let(:initial_verification_request) do
+      {
+        :InitialVerificationRequestSets =>
+        [{ :DHSID =>
+           { :I94UnexpForeignPassportDocumentID =>
+             { :I94Number => "02810190711",
+               :VisaNumber => nil,
+               :PassportNumber => "L1111111",
+               :CountryOfIssuance => "IND",
+               :SEVISID => nil,
+               :DocExpirationDate => Date.new(2030, 12, 25) } },
+           :FirstName => "Jimmy",
+           :MiddleName => nil,
+           :LastName => "Phan",
+           :DateOfBirth => Date.new(1999, 12, 25),
+           :AKA => nil,
+           :FiveYearBarApplicabilityIndicator => true,
+           :RequestSponsorDataIndicator => false,
+           :RequestGrantDateIndicator => true,
+           :SuspectedCounterfeitAlteredDocumentIndicator => false,
+           :RequestCubanHaitianEntrantIndicator => false,
+           :DocumentBinaryAttachment => nil,
+           :RequesterCommentsForHub => nil,
+           :CasePOCFullName => "DHS Tester",
+           :CasePOCPhoneNumber => "5555555555",
+           :CasePOCPhoneNumberExtension => "444" }]
+      }
+    end
+
+    let(:initial_verification_request_entity) do
+      AcaEntities::Fdsh::Vlp::H92::InitialVerificationRequest.call(initial_verification_request)
+    end
+
+    subject do
+      described_class.new.call(initial_verification_request_entity)
+    end
+
+    it "is successful" do
+      expect(subject.success?).to be_truthy
+    end
   end
 
-  let(:initial_verification_request) do
-    {
-      :InitialVerificationRequestSets =>
-      [{ :DHSID =>
-         { :I94UnexpForeignPassportDocumentID =>
-           { :I94Number => "02810190711",
-             :VisaNumber => nil,
-             :PassportNumber => "L1111111",
-             :CountryOfIssuance => "IND",
-             :SEVISID => nil,
-             :DocExpirationDate => Date.new(2030, 12, 25) } },
-         :FirstName => "Jimmy",
-         :MiddleName => nil,
-         :LastName => "Phan",
-         :DateOfBirth => Date.new(1999, 12, 25),
-         :AKA => nil,
-         :FiveYearBarApplicabilityIndicator => true,
-         :RequestSponsorDataIndicator => false,
-         :RequestGrantDateIndicator => true,
-         :SuspectedCounterfeitAlteredDocumentIndicator => false,
-         :RequestCubanHaitianEntrantIndicator => false,
-         :DocumentBinaryAttachment => nil,
-         :RequesterCommentsForHub => nil,
-         :CasePOCFullName => "DHS Tester",
-         :CasePOCPhoneNumber => "5555555555",
-         :CasePOCPhoneNumberExtension => "444" }]
-    }
-  end
+  context "for document TempI551DocumentId" do
+    let(:initial_verification_request) do
+      {
+        :InitialVerificationRequestSets =>
+        [{ :DHSID =>
+           { :TempI551DocumentID =>
+             { :AlienNumber => "908765439",
+               :DocExpirationDate => nil } },
+           :FirstName => "Jimmy",
+           :MiddleName => nil,
+           :LastName => "Phan",
+           :DateOfBirth => Date.new(1999, 12, 25),
+           :AKA => nil,
+           :FiveYearBarApplicabilityIndicator => true,
+           :RequestSponsorDataIndicator => false,
+           :RequestGrantDateIndicator => true,
+           :SuspectedCounterfeitAlteredDocumentIndicator => false,
+           :RequestCubanHaitianEntrantIndicator => false,
+           :DocumentBinaryAttachment => nil,
+           :RequesterCommentsForHub => nil,
+           :CasePOCFullName => "DHS Tester",
+           :CasePOCPhoneNumber => "5555555555",
+           :CasePOCPhoneNumberExtension => "444" }]
+      }
+    end
 
-  subject do
-    described_class.new.call(initial_verification_request_entity)
-  end
+    context "without PassportNumber and CountryOfIssuance" do
+      let(:initial_verification_request_entity) do
+        AcaEntities::Fdsh::Vlp::H92::InitialVerificationRequest.call(initial_verification_request)
+      end
 
-  it "is successful" do
-    expect(subject.success?).to be_truthy
+      subject do
+        described_class.new.call(initial_verification_request_entity)
+      end
+
+      it "is successful" do
+        expect(subject.success?).to be_truthy
+      end
+    end
+
+    context "with nil PassportCountry" do
+      let(:initial_verification_request_entity) do
+        AcaEntities::Fdsh::Vlp::H92::InitialVerificationRequest.call(initial_verification_request_2)
+      end
+
+      let(:initial_verification_request_2) do
+        initial_verification_request[:InitialVerificationRequestSets][0][:DHSID][:TempI551DocumentID][:PassportCountry] = nil
+        initial_verification_request
+      end
+
+      subject do
+        described_class.new.call(initial_verification_request_entity)
+      end
+
+      it "is successful" do
+        expect(subject.success?).to be_truthy
+      end
+    end
   end
 end
