876ddf6f07cfd4b8e9c1d956d6c44342ce80fccc
Yosef Serkez <36456900+4till2@users.noreply.github.com>
2023-11-08 14:30:06 -0500

    create ConsumerRoleReference struct

--- lib/aca_entities/people/resident_role_reference.rb
+++ lib/aca_entities/people/resident_role_reference.rb
@@ -3,7 +3,10 @@
 module AcaEntities
   module People
     class ResidentRoleReference < Dry::Struct
-
+      attribute :is_applicant, Types::Bool.optional.meta(omittable: true)
+      attribute :is_active, Types::Bool.optional.meta(omittable: false)
+      attribute :is_state_resident, Types::Bool.optional.meta(omittable: false)
+      attribute :residency_determined_at, Types::Date.optional.meta(omittable: true)
     end
   end
 end
