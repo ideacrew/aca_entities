d2b5ea2276a15db195e878dbb3694e04933c5d73
Raghu Ram <raghuramg83@gmail.com>
2023-12-18 11:49:04 -0500

    add some comments

--- lib/aca_entities/event_logs/contracts/benefit_sponsorship_event_log_contract.rb
+++ lib/aca_entities/event_logs/contracts/benefit_sponsorship_event_log_contract.rb
@@ -6,6 +6,7 @@ module AcaEntities
     class BenefitSponsorshipEventLogContract < Dry::Validation::Contract
 
       params(EventLogContract.schema) do
+        # Fields specific to the resource model can be included in this section, as illustrated below.
         # required(:custom_sponsorship_field).filled(:string)
       end
     end
--- lib/aca_entities/event_logs/contracts/person_event_log_contract.rb
+++ lib/aca_entities/event_logs/contracts/person_event_log_contract.rb
@@ -6,6 +6,7 @@ module AcaEntities
     class PersonEventLogContract < Dry::Validation::Contract
 
       params(EventLogContract.schema) do
+        # Fields specific to the resource model can be included in this section, as illustrated below.
         # required(:custom_person_field).filled(:string)
       end
     end
